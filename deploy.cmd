691621183ab03457a39e4d85e1d979ed61782332
come.to.the.dark.side.cookie@gmail.com <reinhard.pilz@dynatrace.com>
2021-04-22 13:18:27 +0200

    deleted locally used file

--- deploy.cmd
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-go build
-copy /Y terraform-provider-dynatrace.exe C:\Users\Reinhard.Pilz\AppData\Roaming\terraform.d\plugins\dynatrace.com\com\dynatrace\1.0.3\windows_amd64\terraform-provider-dynatrace_v1.0.3.exe
