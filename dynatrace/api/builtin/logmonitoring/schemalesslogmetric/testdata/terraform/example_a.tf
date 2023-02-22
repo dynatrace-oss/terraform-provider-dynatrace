ff454da5c8c0d50cf3fd60931073a0201b2da7bd
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-02-22 10:01:25 -0500

    Updated log metrics test cases

--- dynatrace/api/builtin/logmonitoring/schemalesslogmetric/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/logmonitoring/schemalesslogmetric/testdata/terraform/example_a.tf
@@ -1,7 +1,7 @@
 resource "dynatrace_log_metrics" "#name#" {
   enabled           = true
   dimensions        = [ "dt.os.type", "dt.entity.process_group" ]
-  key               = "log.terraformexample"
+  key               = "log.terraformexample3"
   measure           = "ATTRIBUTE"
   measure_attribute = "dt.entity.host"
   query             = "TestMatcherValue"
