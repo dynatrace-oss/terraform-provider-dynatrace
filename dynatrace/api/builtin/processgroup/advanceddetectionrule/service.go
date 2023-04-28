1d6d4fb12fdfa4aa63394fad9ae5883d9b1d71a8
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-04-28 13:23:40 -0400

    Added functionality to reject/hijack duplicates on `dynatrace_process_group_detection`

--- dynatrace/api/builtin/processgroup/advanceddetectionrule/service.go
+++ dynatrace/api/builtin/processgroup/advanceddetectionrule/service.go
@@ -18,6 +18,9 @@
 package advanceddetectionrule
 
 import (
+	"fmt"
+
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api"
 	advanceddetectionrule "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/processgroup/advanceddetectionrule/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
@@ -27,5 +30,38 @@ const SchemaVersion = "0.0.5"
 const SchemaID = "builtin:process-group.advanced-detection-rule"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*advanceddetectionrule.Settings] {
-	return settings20.Service[*advanceddetectionrule.Settings](credentials, SchemaID, SchemaVersion)
+	return settings20.Service(credentials, SchemaID, SchemaVersion, &settings20.ServiceOptions[*advanceddetectionrule.Settings]{Duplicates: Duplicates})
+}
+
+func Duplicates(service settings.RService[*advanceddetectionrule.Settings], v *advanceddetectionrule.Settings) (*api.Stub, error) {
+	if settings.RejectDuplicate("dynatrace_process_group_detection") {
+		var err error
+		var stubs api.Stubs
+		if stubs, err = service.List(); err != nil {
+			return nil, err
+		}
+		for _, stub := range stubs {
+			config := stub.Value.(*advanceddetectionrule.Settings)
+			if v.ProcessDetection.Property == config.ProcessDetection.Property && v.ProcessDetection.ContainedString == config.ProcessDetection.ContainedString {
+				if (v.ProcessDetection.RestrictToProcessType == nil && config.ProcessDetection.RestrictToProcessType == nil) || (v.ProcessDetection.RestrictToProcessType != nil && config.ProcessDetection.RestrictToProcessType != nil && *v.ProcessDetection.RestrictToProcessType == *config.ProcessDetection.RestrictToProcessType) {
+					return nil, fmt.Errorf("Advanced detection rule with condition already exists")
+				}
+			}
+		}
+	} else if settings.HijackDuplicate("dynatrace_process_group_detection") {
+		var err error
+		var stubs api.Stubs
+		if stubs, err = service.List(); err != nil {
+			return nil, err
+		}
+		for _, stub := range stubs {
+			config := stub.Value.(*advanceddetectionrule.Settings)
+			if v.ProcessDetection.Property == config.ProcessDetection.Property && v.ProcessDetection.ContainedString == config.ProcessDetection.ContainedString {
+				if (v.ProcessDetection.RestrictToProcessType == nil && config.ProcessDetection.RestrictToProcessType == nil) || (v.ProcessDetection.RestrictToProcessType != nil && config.ProcessDetection.RestrictToProcessType != nil && *v.ProcessDetection.RestrictToProcessType == *config.ProcessDetection.RestrictToProcessType) {
+					return stub, nil
+				}
+			}
+		}
+	}
+	return nil, nil
 }
