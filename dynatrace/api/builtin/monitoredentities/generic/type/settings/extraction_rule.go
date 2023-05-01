500792cab3a7a841e9baa88192b1624dfea2a1d7
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-05-01 09:26:39 -0400

    Updated `dynatrace_generic_types` schema

--- dynatrace/api/builtin/monitoredentities/generic/type/settings/extraction_rule.go
+++ dynatrace/api/builtin/monitoredentities/generic/type/settings/extraction_rule.go
@@ -46,11 +46,11 @@ func (me *ExtractionRules) UnmarshalHCL(decoder hcl.Decoder) error {
 
 // Entity extraction rule. An extraction rule defines which sources are evaluated for extracting an entity. If a source matches the specified filters, an entity is extracted.
 type ExtractionRule struct {
-	Attributes          AttributeEntries `json:"attributes"`                    // All attribute extraction rules will be applied and found attributes will be added to the extracted type.
+	Attributes          AttributeEntries `json:"attributes,omitempty"`          // All attribute extraction rules will be applied and found attributes will be added to the extracted type.
 	IconPattern         *string          `json:"iconPattern,omitempty"`         // Define a pattern which is used to set the icon attribute of the entity. The extracted values must reference barista icon ids. You may define placeholders referencing data source dimensions.
 	IdPattern           string           `json:"idPattern"`                     // ID patterns are comprised of static text and placeholders referring to dimensions in the ingest data. An ID pattern **must** contain at least one placeholder to ensure that different entities will be created.. Take care that the pattern results in the same ID for the same entity. For example, using timestamp or counter-like dimensions as part of the ID would lead to the creation of new entities for each ingest data and is strongly discouraged!\n\nEach dimension key referred to by an identifier placeholder must be present in order to extract an entity. If any dimension key referred to in the identifier is missing, the rule will not be considered for evaluation. If you have cases where you still want to extract the same entity type but have differently named keys, consider creating multiple rules extracting the same entity type. In this case take care that each ID pattern evaluates to the same value if the same entity should be extracted.
 	InstanceNamePattern *string          `json:"instanceNamePattern,omitempty"` // Define a pattern which is used to set the name attribute of the entity. You may define placeholders referencing data source dimensions.
-	RequiredDimensions  DimensionFilters `json:"requiredDimensions"`            // In addition to the dimensions already referred to in the ID pattern, you may specify additional dimensions which must be present in order to evaluate this rule.
+	RequiredDimensions  DimensionFilters `json:"requiredDimensions,omitempty"`  // In addition to the dimensions already referred to in the ID pattern, you may specify additional dimensions which must be present in order to evaluate this rule.
 	Role                *string          `json:"role,omitempty"`                // If you want to extract multiple entities of the same type from a single ingest line you need to define multiple rules with different roles.
 	Sources             SourceFilters    `json:"sources"`                       // Specify all sources which should be evaluated for this rule. A rule is evaluated if any of the specified source filters match.
 }
@@ -60,11 +60,10 @@ func (me *ExtractionRule) Schema() map[string]*schema.Schema {
 		"attributes": {
 			Type:        schema.TypeList,
 			Description: "All attribute extraction rules will be applied and found attributes will be added to the extracted type.",
-			Required:    true,
-
-			Elem:     &schema.Resource{Schema: new(AttributeEntries).Schema()},
-			MinItems: 1,
-			MaxItems: 1,
+			Optional:    true,
+			Elem:        &schema.Resource{Schema: new(AttributeEntries).Schema()},
+			MinItems:    1,
+			MaxItems:    1,
 		},
 		"icon_pattern": {
 			Type:        schema.TypeString,
@@ -84,11 +83,10 @@ func (me *ExtractionRule) Schema() map[string]*schema.Schema {
 		"required_dimensions": {
 			Type:        schema.TypeList,
 			Description: "In addition to the dimensions already referred to in the ID pattern, you may specify additional dimensions which must be present in order to evaluate this rule.",
-			Required:    true,
-
-			Elem:     &schema.Resource{Schema: new(DimensionFilters).Schema()},
-			MinItems: 1,
-			MaxItems: 1,
+			Optional:    true,
+			Elem:        &schema.Resource{Schema: new(DimensionFilters).Schema()},
+			MinItems:    1,
+			MaxItems:    1,
 		},
 		"role": {
 			Type:        schema.TypeString,
@@ -99,10 +97,9 @@ func (me *ExtractionRule) Schema() map[string]*schema.Schema {
 			Type:        schema.TypeList,
 			Description: "Specify all sources which should be evaluated for this rule. A rule is evaluated if any of the specified source filters match.",
 			Required:    true,
-
-			Elem:     &schema.Resource{Schema: new(SourceFilters).Schema()},
-			MinItems: 1,
-			MaxItems: 1,
+			Elem:        &schema.Resource{Schema: new(SourceFilters).Schema()},
+			MinItems:    1,
+			MaxItems:    1,
 		},
 	}
 }
