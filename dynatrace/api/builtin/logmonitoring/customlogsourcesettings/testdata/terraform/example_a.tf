9963d7ac4b73ba65170e56f9bfddb3be3f072900
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-06-08 11:00:40 -0400

    Updated `dynatrace_log_custom_source` schema

--- dynatrace/api/builtin/logmonitoring/customlogsourcesettings/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/logmonitoring/customlogsourcesettings/testdata/terraform/example_a.tf
@@ -3,6 +3,7 @@ resource "dynatrace_log_custom_source" "#name#" {
   enabled = false
   scope   = "HOST_GROUP-1234567890000000"
   custom_log_source {
+    accept_binary = true
     type   = "LOG_PATH_PATTERN"
     values = [ "/terraform" ]
   }
