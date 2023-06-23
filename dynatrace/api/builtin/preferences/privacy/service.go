1a80c5f7714020f72434edeef9a12a3f325046f8
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-06-23 11:20:21 -0400

    Updated resources with version update only

--- dynatrace/api/builtin/preferences/privacy/service.go
+++ dynatrace/api/builtin/preferences/privacy/service.go
@@ -23,7 +23,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "3.0.5"
+const SchemaVersion = "3.0.6"
 const SchemaID = "builtin:preferences.privacy"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*privacy.Settings] {
