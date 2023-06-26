a47a9c0e3415deb6bc68504012d31d69a745d26c
David Cryans <david.cryans@dynatrace.com>
2023-06-26 16:00:49 -0400

    Add the missing parts to complete import-v2

--- dynatrace/export/environment.go
+++ dynatrace/export/environment.go
@@ -18,10 +18,12 @@
 package export
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path"
 	"path/filepath"
 	"sort"
@@ -34,8 +36,11 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/cache"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/shutdown"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/provider/version"
+	"github.com/google/uuid"
 	"github.com/spf13/afero"
 )
+
+var NO_REFRESH_ON_IMPORT = os.Getenv("DYNATRACE_NO_REFRESH_ON_IMPORT") == "true"
 
 type Environment struct {
 	mu                    sync.Mutex
@@ -556,7 +561,7 @@ func (me *Environment) executeImportV2() error {
 		Version:           4,
 		Terraform_version: "1.4.5",
 		Serial:            0,
-		Lineage:           "CREATED_BY_DYNATRACE",
+		Lineage:           uuid.NewString(),
 		Outputs:           nil,
 		Resources:         resources{},
 		CheckResults:      nil,
@@ -580,25 +585,72 @@ func (me *Environment) executeImportV2() error {
 		return err
 	}
 
+	if NO_REFRESH_ON_IMPORT {
+		fmt.Println("NO_REFRESH_ON_IMPORT set to true")
+	} else {
+		me.executeTF(genPlanCmd())
+		me.executeTF(genApplyCmd())
+	}
+
 	return nil
 }
 
-func updateState(newStateObject interface{}, stateObject interface{}) interface{} {
-	if stateObject == nil {
-		return newStateObject
+func genPlanCmd() *exec.Cmd {
+	fmt.Println("Refreshing State - Plan ...")
+	exePath, _ := exec.LookPath("terraform")
+
+	return exec.Command(
+		exePath,
+		"plan",
+		"-lock=false",
+		"-parallelism=50",
+		"-refresh-only",
+		"-no-color",
+		"-out=terraform.plan",
+	)
+}
+
+func genApplyCmd() *exec.Cmd {
+	fmt.Println("Refreshing State - Apply ...")
+	exePath, _ := exec.LookPath("terraform")
+
+	return exec.Command(
+		exePath,
+		"apply",
+		"-lock=false",
+		"-parallelism=50",
+		"-refresh-only",
+		"-no-color",
+		"-auto-approve",
+		"terraform.plan",
+	)
+}
+
+func (me *Environment) executeTF(cmd *exec.Cmd) (err error) {
+
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	cmd.Dir = me.OutputFolder
+	var cacheFolder string
+	if cacheFolder, err = filepath.Abs(cache.GetCacheFolder()); err != nil {
+		return err
+	}
+	cmd.Env = []string{
+		// "TF_LOG_PROVIDER=INFO",
+		"DYNATRACE_ENV_URL=" + me.Credentials.URL,
+		"DYNATRACE_API_TOKEN=" + me.Credentials.Token,
+		"DT_CACHE_FOLDER=" + cacheFolder,
+		"CACHE_OFFLINE_MODE=true",
+		"DT_CACHE_DELETE_ON_LAUNCH=false",
+		"DT_NO_CACHE_CLEANUP=true",
+		"DT_TERRAFORM_IMPORT=true",
+	}
+	cmd.Start()
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("out:", outb.String())
+		fmt.Println("err:", errb.String())
 	}
 
-	if newStateObject == nil {
-		newStateObject = stateObject
-	} else {
-		serialValue := stateObject.(map[string]interface{})["serial"].(float64)
-		newSerialValue := newStateObject.(map[string]interface{})["serial"].(float64)
-		newStateObject.(map[string]interface{})["serial"] = (serialValue + newSerialValue)
-
-		newStateObject.(map[string]interface{})["resources"] = append(
-			newStateObject.(map[string]interface{})["resources"].([]interface{}),
-			stateObject.(map[string]interface{})["resources"].([]interface{})...,
-		)
-	}
-	return newStateObject
+	return nil
 }
