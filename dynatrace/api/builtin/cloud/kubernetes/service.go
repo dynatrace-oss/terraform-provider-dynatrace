54515067b6b87cb3dce4ec079264ed7391abb1a1
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-07-06 11:50:36 -0400

    Updated `dynatrace_kubernetes` schema

--- dynatrace/api/builtin/cloud/kubernetes/service.go
+++ dynatrace/api/builtin/cloud/kubernetes/service.go
@@ -23,7 +23,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "2.5"
+const SchemaVersion = "3"
 const SchemaID = "builtin:cloud.kubernetes"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*kubernetes.Settings] {
