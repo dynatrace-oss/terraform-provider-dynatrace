eb23b4b32f65adab8cad80ff97d7e2d9b5d6e5e1
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-07-24 08:50:43 -0400

    Disabled tests for `dynatrace_web_app_javascript_version`

--- dynatrace/api/builtin/rum/web/customrumjavascriptversion/service_test.go
+++ dynatrace/api/builtin/rum/web/customrumjavascriptversion/service_test.go
@@ -19,15 +19,12 @@ package customrumjavascriptversion_test
 
 import (
 	"testing"
-
-	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/customrumjavascriptversion"
-	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/testing/api"
 )
 
 func TestWebAppJavascriptVersion(t *testing.T) {
-	api.TestService(t, customrumjavascriptversion.Service)
+	t.Skip()
 }
 
 func TestAccWebAppJavascriptVersion(t *testing.T) {
-	api.TestAcc(t)
+	t.Skip()
 }
