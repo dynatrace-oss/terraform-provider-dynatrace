3227a5bc05077dfb4dd8e836254856793e7d2042
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-08-09 09:39:04 -0400

    Updated `dynatrace_process_monitoring_rule` schema

--- dynatrace/api/builtin/process/monitoring/custom/service.go
+++ dynatrace/api/builtin/process/monitoring/custom/service.go
@@ -26,7 +26,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.4.4"
+const SchemaVersion = "0.4.5"
 const SchemaID = "builtin:process.custom-process-monitoring-rule"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*customprocessmonitoring.Settings] {
