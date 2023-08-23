7ba8260ed07d396717a392caf664d01839c26437
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2023-08-23 14:44:49 -0400

    Updated resources with version update only

--- dynatrace/api/builtin/oneagent/features/service.go
+++ dynatrace/api/builtin/oneagent/features/service.go
@@ -23,7 +23,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.5.7"
+const SchemaVersion = "1.5.9"
 const SchemaID = "builtin:oneagent.features"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*features.Settings] {
