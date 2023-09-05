57a7754454448de3ae1ffcc579acc9878347e5fa
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2023-09-05 11:38:36 +0200

    Fixed #325

--- dynatrace/api/iam/oauth_client.go
+++ dynatrace/api/iam/oauth_client.go
@@ -2,12 +2,16 @@ package iam
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"sync"
+
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 )
 
 type Authenticator interface {
@@ -26,6 +30,8 @@ type oauthResponse struct {
 }
 
 var mutex *sync.Mutex = new(sync.Mutex)
+
+var msgInvalidOAuthCredentials = "Invalid OAuth credentials"
 
 func getBearer(auth Authenticator, forceNew bool) (string, error) {
 	mutex.Lock()
@@ -62,6 +68,21 @@ func getBearer(auth Authenticator, forceNew bool) (string, error) {
 	}
 	if body, err = io.ReadAll(httpRes.Body); err != nil {
 		return "", err
+	}
+	if httpRes.StatusCode == 400 {
+		if os.Getenv("DT_DEBUG_IAM_BEARER") == "true" {
+			debugPayloadStr := fmt.Sprintf(
+				"grant_type=client_credentials&client_id=%s&client_secret=%s&scope=%s&resource=%s",
+				url.QueryEscape(auth.ClientID()),
+				url.QueryEscape("<hidden>"),
+				url.QueryEscape("account-idm-read account-idm-write iam:policies:read iam:policies:write iam-policies-management"),
+				url.QueryEscape("urn:dtaccount:"+strings.TrimPrefix(auth.AccountID(), "urn:dtaccount:")),
+			)
+			rest.Logger.Println("POST https://sso.dynatrace.com/sso/oauth2/token")
+			rest.Logger.Println("  " + debugPayloadStr)
+			rest.Logger.Println("  -> " + string(body))
+		}
+		return "", errors.New(msgInvalidOAuthCredentials)
 	}
 	response := oauthResponse{}
 	if err = json.Unmarshal(body, &response); err != nil {
