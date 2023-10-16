369bd50b8b88588bd90912bb846ee6825017f34a
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2023-10-16 11:00:02 +0200

    Fixed #339

--- dynatrace/api/v1/config/applications/web/detection/service.go
+++ dynatrace/api/v1/config/applications/web/detection/service.go
@@ -18,6 +18,9 @@
 package detection
 
 import (
+	"time"
+
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api"
 	detection "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/v1/config/applications/web/detection/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 )
@@ -26,9 +29,54 @@ const SchemaID = "v1:config:applications:detection"
 const BasePath = "/api/config/v1/applicationDetectionRules"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*detection.Rule] {
-	return settings.NewCRUDService(
+	return &service{settings.NewCRUDService(
 		credentials,
 		SchemaID,
 		settings.DefaultServiceOptions[*detection.Rule](BasePath),
-	)
+	)}
+}
+
+type service struct {
+	service settings.CRUDService[*detection.Rule]
+}
+
+func (s *service) List() (api.Stubs, error) {
+	return s.service.List()
+}
+
+func (s *service) Get(id string, v *detection.Rule) error {
+	return s.service.Get(id, v)
+}
+
+func (s *service) SchemaID() string {
+	return s.service.SchemaID()
+}
+
+func (s *service) Create(v *detection.Rule) (*api.Stub, error) {
+	stub, err := s.service.Create(v)
+	if err == nil {
+		return stub, err
+	}
+	numRetriesLeft := 6
+	for err != nil && err.Error() == "Unable to persist application detection rule" {
+		numRetriesLeft--
+		if numRetriesLeft < 0 {
+			break
+		}
+		time.Sleep(10 * time.Second)
+		stub, err = s.service.Create(v)
+	}
+	return stub, err
+}
+
+func (s *service) Update(id string, v *detection.Rule) error {
+	return s.service.Update(id, v)
+}
+
+func (s *service) Delete(id string) error {
+	return s.service.Delete(id)
+}
+
+func (s *service) Name() string {
+	return s.service.Name()
 }
