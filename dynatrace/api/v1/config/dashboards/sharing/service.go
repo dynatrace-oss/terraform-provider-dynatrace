8414b3218343d043abdccedb800bc8d3c6fb4454
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2023-10-23 16:26:04 +0200

    Fix: Dashboard Sharing was broken

--- dynatrace/api/v1/config/dashboards/sharing/service.go
+++ dynatrace/api/v1/config/dashboards/sharing/service.go
@@ -28,6 +28,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/cache"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/httpcache"
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/provider/logging"
 
 	dashboards "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/v1/config/dashboards/settings"
 	sharing "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/v1/config/dashboards/sharing/settings"
@@ -126,11 +127,8 @@ func (me *service) update(id string, v *sharing.DashboardSharing, retry int) err
 		return err
 	}
 
-	if err := me.client.Get(fmt.Sprintf("/api/config/v1/dashboards/%s/shareSettings", url.PathEscape(id)), 200).Finish(v); err != nil {
-		return err
-	}
-
 	if dbm.DashboardMetaData.Preset {
+		logging.File.Println("dbm.DashboardMetaData.Preset:", dbm.DashboardMetaData.Preset)
 		v.Muted = true
 		return nil
 	}
