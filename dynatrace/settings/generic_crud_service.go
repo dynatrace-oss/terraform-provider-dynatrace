99b5f182d9971c9d3b3da3034174f07a08f65e8c
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2023-10-24 13:25:35 +0200

    [Improvement] Services are now able to receive the current state upon Get, Create and Update

--- dynatrace/settings/generic_crud_service.go
+++ dynatrace/settings/generic_crud_service.go
@@ -17,7 +17,11 @@
 
 package settings
 
-import "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api"
+import (
+	"context"
+
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api"
+)
 
 type GenericCRUDService[T Settings] struct {
 	Service CRUDService[T]
@@ -34,11 +38,25 @@ func (me *GenericCRUDService[T]) List() (api.Stubs, error) {
 	return me.Service.List()
 }
 
+func (me *GenericCRUDService[T]) GetWithContext(ctx context.Context, id string, v Settings) error {
+	if contextGetter, ok := me.Service.(ContextGetter[T]); ok {
+		return contextGetter.GetWithContext(ctx, id, v.(T))
+	}
+	return me.Service.Get(id, v.(T))
+}
+
 func (me *GenericCRUDService[T]) Get(id string, v Settings) error {
 	return me.Service.Get(id, v.(T))
 }
 
 func (me *GenericCRUDService[T]) Create(v Settings) (*api.Stub, error) {
+	return me.Service.Create(v.(T))
+}
+
+func (me *GenericCRUDService[T]) CreateWithContext(ctx context.Context, v Settings) (*api.Stub, error) {
+	if contextCreator, ok := me.Service.(ContextCreator[T]); ok {
+		return contextCreator.CreateWithContext(ctx, v.(T))
+	}
 	return me.Service.Create(v.(T))
 }
 
@@ -50,6 +68,13 @@ func (me *GenericCRUDService[T]) Validate(v Settings) error {
 }
 
 func (me *GenericCRUDService[T]) Update(id string, v Settings) error {
+	return me.Service.Update(id, v.(T))
+}
+
+func (me *GenericCRUDService[T]) UpdateWithContext(ctx context.Context, id string, v Settings) error {
+	if contextUpdater, ok := me.Service.(ContextUpdater[T]); ok {
+		return contextUpdater.UpdateWithContext(ctx, id, v.(T))
+	}
 	return me.Service.Update(id, v.(T))
 }
 
