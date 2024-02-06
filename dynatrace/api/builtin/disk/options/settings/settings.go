e26b710e0e6fad77df8498c33632cbb332a57650
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-02-06 14:29:57 -0500

    Updated `dynatrace_disk_options` resource

--- dynatrace/api/builtin/disk/options/settings/settings.go
+++ dynatrace/api/builtin/disk/options/settings/settings.go
@@ -23,9 +23,10 @@ import (
 )
 
 type Settings struct {
-	Exclusions DiskComplexes `json:"exclusions,omitempty"` // OneAgent automatically detects and monitors all your mount points, however you can create exception rules to remove disks from the monitoring list.
-	NfsShowAll bool          `json:"nfsShowAll"`           // When disabled OneAgent will try to deduplicate some of nfs disks. Disabled by default, applies only to Linux hosts. Requires OneAgent 1.209 or later
-	Scope      *string       `json:"-" scope:"scope"`      // The scope of this setting (HOST, HOST_GROUP). Omit this property if you want to cover the whole environment.
+	DisableNfsDiskMonitoring bool          `json:"disableNfsDiskMonitoring"` // Deactivate NFS monitoring on all supported systems
+	Exclusions               DiskComplexes `json:"exclusions,omitempty"`     // OneAgent automatically detects and monitors all your mount points, however you can create exception rules to remove disks from the monitoring list.
+	NfsShowAll               bool          `json:"nfsShowAll"`               // When disabled OneAgent will try to deduplicate some of nfs disks. Disabled by default, applies only to Linux hosts. Requires OneAgent 1.209 or later
+	Scope                    *string       `json:"-" scope:"scope"`          // The scope of this setting (HOST, HOST_GROUP). Omit this property if you want to cover the whole environment.
 }
 
 func (me *Settings) Name() string {
@@ -34,10 +35,15 @@ func (me *Settings) Name() string {
 
 func (me *Settings) Schema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
+		"disable_nfs_disk_monitoring": {
+			Type:        schema.TypeBool,
+			Description: "Deactivate NFS monitoring on all supported systems",
+			Optional:    true,
+		},
 		"exclusions": {
 			Type:        schema.TypeList,
 			Description: "OneAgent automatically detects and monitors all your mount points, however you can create exception rules to remove disks from the monitoring list.",
-			Optional:    true,
+			Optional:    true, // minobjects == 0
 			Elem:        &schema.Resource{Schema: new(DiskComplexes).Schema()},
 			MinItems:    1,
 			MaxItems:    1,
@@ -45,30 +51,31 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		"nfs_show_all": {
 			Type:        schema.TypeBool,
 			Description: "When disabled OneAgent will try to deduplicate some of nfs disks. Disabled by default, applies only to Linux hosts. Requires OneAgent 1.209 or later",
-			Required:    true,
+			Optional:    true, // precondition
 		},
 		"scope": {
 			Type:        schema.TypeString,
 			Description: "The scope of this setting (HOST, HOST_GROUP). Omit this property if you want to cover the whole environment.",
 			Optional:    true,
 			Default:     "environment",
-			ForceNew:    true,
 		},
 	}
 }
 
 func (me *Settings) MarshalHCL(properties hcl.Properties) error {
 	return properties.EncodeAll(map[string]any{
-		"exclusions":   me.Exclusions,
-		"nfs_show_all": me.NfsShowAll,
-		"scope":        me.Scope,
+		"disable_nfs_disk_monitoring": me.DisableNfsDiskMonitoring,
+		"exclusions":                  me.Exclusions,
+		"nfs_show_all":                me.NfsShowAll,
+		"scope":                       me.Scope,
 	})
 }
 
 func (me *Settings) UnmarshalHCL(decoder hcl.Decoder) error {
 	return decoder.DecodeAll(map[string]any{
-		"exclusions":   &me.Exclusions,
-		"nfs_show_all": &me.NfsShowAll,
-		"scope":        &me.Scope,
+		"disable_nfs_disk_monitoring": &me.DisableNfsDiskMonitoring,
+		"exclusions":                  &me.Exclusions,
+		"nfs_show_all":                &me.NfsShowAll,
+		"scope":                       &me.Scope,
 	})
 }
