e511047ab17f7401e89533b4b8ab67f0b329a1e8
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-02-06 11:36:16 -0500

    Updated resources with version update only

--- dynatrace/api/builtin/cloud/kubernetes/service.go
+++ dynatrace/api/builtin/cloud/kubernetes/service.go
@@ -23,7 +23,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "3.0.1"
+const SchemaVersion = "3.1"
 const SchemaID = "builtin:cloud.kubernetes"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*kubernetes.Settings] {
--- dynatrace/api/builtin/customunit/service.go
+++ dynatrace/api/builtin/customunit/service.go
@@ -23,7 +23,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.3"
+const SchemaVersion = "0.5"
 const SchemaID = "builtin:custom-unit"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*customunit.Settings] {
