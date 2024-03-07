d3a163bcde494bcf228c789f0fc1cef99bfb0179
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2024-03-07 14:02:55 +0100

    merged monitoring configuration into main settings

--- dynatrace/api/v2/extensions/twozero/service.go
+++ dynatrace/api/v2/extensions/twozero/service.go
@@ -18,10 +18,11 @@
 package twozero
 
 import (
-	"errors"
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api"
 	twozero "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/v2/extensions/twozero/settings"
@@ -38,43 +39,185 @@ type service struct {
 }
 
 func (me *service) Get(id string, v *twozero.Settings) error {
-	name, version := splitID(id)
-	var extensionsList ExtensionsList
+	name, configurationID := splitID(id)
+	var response GetMonitoringConfigurationResponse
 	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
-	if err := client.Get(fmt.Sprintf("/api/v2/extensions/info?name=%s", url.QueryEscape(name)), 200).Finish(&extensionsList); err != nil {
+	if err := client.Get(fmt.Sprintf("/api/v2/extensions/%s/monitoringConfigurations/%s", url.PathEscape(name), url.PathEscape(configurationID)), 200).Finish(&response); err != nil {
 		return err
 	}
-	if len(extensionsList.Extensions) == 0 {
-		return rest.Error{Code: 404, Message: fmt.Sprintf("extension `%s` is not installed", name)}
+	v.Scope = response.Scope
+	v.Name = name
+	v.Value = string(response.Value)
+	return nil
+}
+
+func (me *service) List() (api.Stubs, error) {
+	var stubs api.Stubs
+
+	var extensionsList ExtensionsList
+	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
+
+	if err := client.Get("/api/v2/extensions/info", 200).Finish(&extensionsList); err != nil {
+		return stubs, err
 	}
 	for _, extension := range extensionsList.Extensions {
-		if extension.Name != name {
-			continue
-		}
-		// if we want the version to get computed
-		if len(version) == 0 {
-			v.Name = name
-			v.Version = extension.ActiveVersion
-			return nil
-		}
+		nextPageKey := "first"
+		for len(nextPageKey) > 0 {
+			configList := new(MonitoringConfigurationList)
+			query := "?pageSize=100"
+			if len(nextPageKey) > 0 && nextPageKey != "first" {
+				query = query + "&nextPageKey=" + url.QueryEscape(nextPageKey)
+			}
+			if err := client.Get(fmt.Sprintf("/api/v2/extensions/%s/monitoringConfigurations%s", url.PathEscape(extension.Name), query), 200).Finish(&configList); err != nil {
+				return stubs, err
+			}
+			nextPageKey = configList.NextPageKey
+			for _, config := range configList.Items {
+				stubs = append(stubs, &api.Stub{ID: joinID(extension.Name, config.ObjectID), Name: joinID(extension.Name, config.ObjectID)})
 
-		// if we're looking for a specific version but a different one is active
-		if extension.ActiveVersion != version {
-			return rest.Error{Code: 404, Message: fmt.Sprintf("extension `%s` is installed, but in version %s (expected: %s)", name, extension.ActiveVersion, version)}
+			}
 		}
-
-		// success
-		v.Name = name
-		v.Version = version
-		return nil
 	}
 
-	// If none of the extensions within the response matched
-	return rest.Error{Code: 404, Message: fmt.Sprintf("extension `%s` is not installed", name)}
+	return stubs, nil
+}
+
+func (me *service) Create(v *twozero.Settings) (*api.Stub, error) {
+	version, err := extractVersion(v)
+	if err != nil {
+		return nil, err
+	}
+
+	name := v.Name
+	if err := me.ensureInstalled(name, version); err != nil {
+		return nil, err
+	}
+	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
+	createResponse := []CreateMonitoringConfigResponse{}
+	retry := 10
+	for retry > 0 {
+		payload := []MonitoringConfigCreateDto{{Scope: v.Scope, Value: []byte(v.Value)}}
+		if err := client.Post(fmt.Sprintf("/api/v2/extensions/%s/monitoringConfigurations", url.PathEscape(name)), &payload, 200).Finish(&createResponse); err != nil {
+			if err.Error() == fmt.Sprintf("No schema with identifier 'ext:%s'", name) {
+				time.Sleep(1 * time.Second)
+				retry--
+			} else {
+				return nil, err
+			}
+		} else {
+			retry = 0
+		}
+	}
+	if len(createResponse) == 0 {
+		return nil, fmt.Errorf("creating monitoring config didn't deliver ID")
+	}
+	return &api.Stub{ID: joinID(name, createResponse[0].ObjectID)}, nil
+}
+
+func (me *service) ensureInstalled(name string, version string) error {
+	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
+	response := struct {
+		Name    string `json:"extensionName"`
+		Version string `json:"extensionVersion"`
+	}{}
+	if err := client.Post(fmt.Sprintf("/api/v2/extensions/%s?version=%s", url.PathEscape(name), url.QueryEscape(version)), nil, 200).Finish(&response); err != nil {
+		if restErr, ok := err.(rest.Error); ok {
+			if (restErr.Code == 400) && restErr.Message == fmt.Sprintf("Extension %s has already been added to environment", name) {
+				return nil
+			}
+		}
+		return err
+	}
+	return nil
+}
+
+func (me *service) Update(id string, v *twozero.Settings) error {
+	_, configID := splitID(id)
+	version, err := extractVersion(v)
+	if err != nil {
+		return err
+	}
+	name := v.Name
+	if err := me.ensureInstalled(name, version); err != nil {
+		return err
+	}
+	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
+	createResponse := CreateMonitoringConfigResponse{}
+	payload := MonitoringConfigCreateDto{Value: []byte(v.Value)}
+	if err := client.Put(fmt.Sprintf("/api/v2/extensions/%s/monitoringConfigurations/%s", url.PathEscape(name), url.PathEscape(configID)), &payload, 200).Finish(&createResponse); err != nil {
+		return err
+	}
+	if createResponse.ObjectID != configID {
+		return fmt.Errorf("the ID of the configuration has changed during update")
+	}
+	return nil
+}
+
+func (me *service) Delete(id string) error {
+	name, configID := splitID(id)
+	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
+	if err := client.Delete(fmt.Sprintf("/api/v2/extensions/%s/monitoringConfigurations/%s", url.PathEscape(name), url.PathEscape(configID)), 200).Finish(nil); err != nil {
+		if restErr, ok := err.(rest.Error); ok {
+			if restErr.Code != 404 {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func (me *service) SchemaID() string {
 	return "v2:extensions:twozero"
+}
+
+func (me *service) Validate(v *twozero.Settings) error {
+	return nil // no endpoint for that
+}
+
+func (me *service) New() *twozero.Settings {
+	return new(twozero.Settings)
+}
+
+func (me *service) Name() string {
+	return me.SchemaID()
+}
+
+func extractVersion(v *twozero.Settings) (string, error) {
+	valueMap := map[string]any{}
+	if err := json.Unmarshal([]byte(v.Value), &valueMap); err != nil {
+		return "", err
+	}
+	version := ""
+	if untypedVersion, ok := valueMap["version"]; ok {
+		if sVersion, ok := untypedVersion.(string); ok {
+			version = sVersion
+		}
+	}
+	if len(version) == 0 {
+		return "", fmt.Errorf("the value of the monitoring configuration doesn't contain a 'version'")
+	}
+	return version, nil
+}
+
+func joinID(name string, configurationID string) string {
+	return fmt.Sprintf("%s#-#%s", name, configurationID)
+}
+
+func splitID(id string) (name string, configurationID string) {
+	parts := strings.Split(id, "#-#")
+	if len(parts) > 0 {
+		name = parts[0]
+	}
+	if len(parts) > 1 {
+		configurationID = parts[1]
+	}
+	return
+}
+
+type GetMonitoringConfigurationResponse struct {
+	ObjectID string          `json:"objectId"`
+	Scope    string          `json:"scope"`
+	Value    json.RawMessage `json:"value"`
 }
 
 type ExtensionsList struct {
@@ -85,144 +228,22 @@ type ExtensionsList struct {
 	} `json:"extensions"`
 }
 
-func joinID(name string, version string) string {
-	return fmt.Sprintf("%s#-#%s", name, version)
+type MonitoringConfigurationList struct {
+	Items []struct {
+		ObjectID string `json:"objectId"`
+		Scope    string `json:"scope"`
+		Value    struct {
+			Version string `json:"version"`
+		} `json:"value"`
+	} `json:"items"`
+	NextPageKey string `json:"nextPageKey"`
 }
 
-func splitID(id string) (name string, version string) {
-	parts := strings.Split(id, "#-#")
-	if len(parts) > 0 {
-		name = parts[0]
-	}
-	if len(parts) > 1 {
-		version = parts[1]
-	}
-	return
+type MonitoringConfigCreateDto struct {
+	Scope string          `json:"scope,omitempty"`
+	Value json.RawMessage `json:"value"`
 }
 
-func (me *service) List() (api.Stubs, error) {
-	return me.list("")
-}
-
-func (me *service) list(exname string) (api.Stubs, error) {
-	var stubs api.Stubs
-
-	var extensionsList ExtensionsList
-	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
-	query := ""
-	if len(exname) > 0 {
-		query = fmt.Sprintf("?name=%s", url.QueryEscape(exname))
-	}
-	if err := client.Get(fmt.Sprintf("/api/v2/extensions/info%s", query), 200).Finish(&extensionsList); err != nil {
-		return stubs, err
-	}
-	for _, extension := range extensionsList.Extensions {
-		if len(exname) > 0 && extension.Name != exname {
-			continue
-		}
-		name := extension.Name
-		version := extension.ActiveVersion
-		stubs = append(stubs, &api.Stub{ID: joinID(name, version), Name: joinID(name, version)})
-	}
-
-	return stubs, nil
-}
-
-func (me *service) Validate(v *twozero.Settings) error {
-	return nil // no endpoint for that
-}
-
-func (me *service) Create(v *twozero.Settings) (*api.Stub, error) {
-	stub, err := me.ensureInstalled(v)
-	if err != nil {
-		return stub, err
-	}
-	name, version := splitID(stub.ID)
-	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
-	payload := struct {
-		Version string `json:"version"`
-	}{
-		Version: version,
-	}
-	return stub, client.Put(fmt.Sprintf("/api/v2/extensions/%s/environmentConfiguration", url.PathEscape(name)), &payload, 200).Finish(nil)
-}
-
-func (me *service) ensureInstalled(v *twozero.Settings) (*api.Stub, error) {
-	name := v.Name
-	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
-	response := struct {
-		Name    string `json:"extensionName"`
-		Version string `json:"extensionVersion"`
-	}{}
-	query := ""
-	if len(v.Version) > 0 {
-		query = "?version=" + url.QueryEscape(v.Version)
-	}
-	if err := client.Post(fmt.Sprintf("/api/v2/extensions/%s%s", url.PathEscape(name), query), nil, 200).Finish(&response); err != nil {
-		if restErr, ok := err.(rest.Error); ok {
-			if (restErr.Code == 400) && restErr.Message == fmt.Sprintf("Extension %s has already been added to environment", name) {
-				stubs, err := me.list(name)
-				if err != nil {
-					return nil, err
-				}
-				if len(stubs) == 0 {
-					return nil, rest.Error{Code: 404, Message: fmt.Sprintf("unable to find existing settings for extension %s", name)}
-				}
-				return stubs[0], nil
-			}
-		}
-		return &api.Stub{}, err
-	}
-	return &api.Stub{ID: joinID(response.Name, response.Version), Name: joinID(response.Name, response.Version)}, nil
-}
-
-func (me *service) Update(id string, v *twozero.Settings) error {
-	return errors.New("an update for this resource is not supposed to happen")
-	// name, _ := splitID(id)
-	// client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
-	// payload := struct {
-	// 	Version string `json:"version"`
-	// }{
-	// 	Version: v.Version,
-	// }
-	// if err := client.Put(fmt.Sprintf("/api/v2/extensions/%s/environmentConfiguration", url.PathEscape(name)), &payload, 200).Finish(nil); err != nil {
-	// 	if restErr, ok := err.(rest.Error); ok {
-	// 		// Reasoning: We may want to update to a different version, but that version is not necessarily installed
-	// 		if restErr.Code == 404 && restErr.Message == fmt.Sprintf("Extension %s, version %s not found or operation forbidden", name, v.Version) {
-	// 			if _, err := me.ensureInstalled(v); err != nil {
-	// 				return err
-	// 			}
-	// 			return me.Update(id, v)
-	// 		}
-	// 	}
-	// }
-	// return nil
-}
-
-func (me *service) Delete(id string) error {
-	name, version := splitID(id)
-	client := rest.DefaultClient(me.credentials.URL, me.credentials.Token)
-	if err := client.Delete(fmt.Sprintf("/api/v2/extensions/%s/environmentConfiguration", url.PathEscape(name)), 200).Finish(nil); err != nil {
-		if restErr, ok := err.(rest.Error); ok {
-			if restErr.Code != 404 {
-				return err
-			}
-		}
-	}
-	if err := client.Delete(fmt.Sprintf("/api/v2/extensions/%s/%s", url.PathEscape(name), url.PathEscape(version)), 200).Finish(nil); err != nil {
-		if restErr, ok := err.(rest.Error); ok {
-			if restErr.Code != 404 {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func (me *service) New() *twozero.Settings {
-	return new(twozero.Settings)
-}
-
-func (me *service) Name() string {
-	return me.SchemaID()
+type CreateMonitoringConfigResponse struct {
+	ObjectID string `json:"objectId"`
 }
--- dynatrace/api/v2/extensions/twozero/settings/settings.go
+++ dynatrace/api/v2/extensions/twozero/settings/settings.go
@@ -18,21 +18,18 @@
 package twozero
 
 import (
-	"errors"
-	"fmt"
-	"regexp"
-
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/terraform/hcl"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 type Settings struct {
-	Name    string `json:"-"`
-	Version string `json:"-"`
+	Name  string `json:"-"`
+	Value string `json:"-"`
+	Scope string `json:"-"`
 }
 
-var reg = regexp.MustCompile(`^\d+\.\d+\.\d+$`)
+// var reg = regexp.MustCompile(`^\d+\.\d+\.\d+$`)
 
 func (me *Settings) Schema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
@@ -42,38 +39,51 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 			ForceNew:    true,
 			Required:    true,
 		},
-		"version": {
+		"scope": {
 			Type:        schema.TypeString,
-			Description: "The fully qualified name of the extension, such as `com.dynatrace.extension.jmx-liberty-cp`",
-			ValidateFunc: func(i any, k string) (warnings []string, errs []error) {
-				v, ok := i.(string)
-				if !ok {
-					errs = append(errs, fmt.Errorf("expected type of %s to be string", k))
-					return warnings, errs
-				}
-				if !reg.MatchString(v) {
-					errs = append(errs, errors.New("expected format of %s to be `MAJOR.MINOR.REVISION` (e.g. `1.0.0`)"))
-					return warnings, errs
-				}
-				return warnings, errs
-			},
-			Optional: true,
-			Computed: true,
-			ForceNew: true,
+			Description: "The scope this monitoring configuration will be defined for. This can be either a Host, a Host Group, a Management Zone or an Active Gate Group.",
+			ForceNew:    true,
+			Required:    true,
 		},
+		"value": {
+			Type:        schema.TypeString,
+			Description: "The JSON encoded value for this monitoring configuration",
+			Required:    true,
+		},
+		// "version": {
+		// 	Type:        schema.TypeString,
+		// 	Description: "The fully qualified name of the extension, such as `com.dynatrace.extension.jmx-liberty-cp`",
+		// 	ValidateFunc: func(i any, k string) (warnings []string, errs []error) {
+		// 		v, ok := i.(string)
+		// 		if !ok {
+		// 			errs = append(errs, fmt.Errorf("expected type of %s to be string", k))
+		// 			return warnings, errs
+		// 		}
+		// 		if !reg.MatchString(v) {
+		// 			errs = append(errs, errors.New("expected format of %s to be `MAJOR.MINOR.REVISION` (e.g. `1.0.0`)"))
+		// 			return warnings, errs
+		// 		}
+		// 		return warnings, errs
+		// 	},
+		// 	Optional: true,
+		// 	Computed: true,
+		// 	ForceNew: true,
+		// },
 	}
 }
 
 func (me *Settings) MarshalHCL(properties hcl.Properties) error {
 	return properties.EncodeAll(map[string]any{
-		"name":    me.Name,
-		"version": me.Version,
+		"name":  me.Name,
+		"value": me.Value,
+		"scope": me.Scope,
 	})
 }
 
 func (me *Settings) UnmarshalHCL(decoder hcl.Decoder) error {
 	return decoder.DecodeAll(map[string]any{
-		"name":    &me.Name,
-		"version": &me.Version,
+		"name":  &me.Name,
+		"value": &me.Value,
+		"scope": &me.Scope,
 	})
 }
