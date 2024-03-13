2230947326c891b95ace2c0eaea103fd2984842e
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2024-03-13 12:21:15 +0100

    Fix: `insert_after` attributes need to be flagged as `optional` AND `computed`

--- dynatrace/api/builtin/logmonitoring/logdpprules/settings/settings.go
+++ dynatrace/api/builtin/logmonitoring/logdpprules/settings/settings.go
@@ -78,6 +78,7 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 			Type:        schema.TypeString,
 			Description: "Because this resource allows for ordering you may specify the ID of the resource instance that comes before this instance regarding order. If not specified when creating the setting will be added to the end of the list. If not specified during update the order will remain untouched",
 			Optional:    true,
+			Computed:    true,
 		},
 	}
 }
