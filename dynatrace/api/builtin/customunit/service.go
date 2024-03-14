6c02c1c8ca31c8ffbe2f785c679793d1c3f0c2ee
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-03-14 14:00:19 -0400

    Updated resources with version update only

--- dynatrace/api/builtin/customunit/service.go
+++ dynatrace/api/builtin/customunit/service.go
@@ -23,7 +23,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.5"
+const SchemaVersion = "0.6"
 const SchemaID = "builtin:custom-unit"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*customunit.Settings] {
