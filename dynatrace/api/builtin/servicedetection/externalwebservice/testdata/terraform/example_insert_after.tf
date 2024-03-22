9879ee5d58fa9e1fd9629422a584d0b0b2be80df
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2024-03-22 11:30:02 +0100

    Update example_insert_after.tf

--- dynatrace/api/builtin/servicedetection/externalwebservice/testdata/terraform/example_insert_after.tf
+++ dynatrace/api/builtin/servicedetection/externalwebservice/testdata/terraform/example_insert_after.tf
@@ -20,7 +20,7 @@ resource "dynatrace_management_zone_v2" "my-mgmz" {
   }
 }
 
-resource "dynatrace_service_external_web_service" "#name#" {
+resource "dynatrace_service_external_web_service" "first-instance" {
   name             = "#name#"
   description      = "Created by Terraform"
   enabled          = false
@@ -46,4 +46,33 @@ resource "dynatrace_service_external_web_service" "#name#" {
       }
     }
   }
+}
+
+resource "dynatrace_service_external_web_service" "second-instance" {
+  name             = "#name#-second"
+  description      = "Created by Terraform"
+  enabled          = false
+  management_zones = [ dynatrace_management_zone_v2.my-mgmz.id ]
+  conditions {
+    condition {
+      attribute              = "UrlPath"
+      compare_operation_type = "StringContains"
+      ignore_case            = true
+      text_values            = [ "Terraform-3" ]
+    }
+  }
+  id_contributors {
+    detect_as_web_request_service = false
+    port_for_service_id           = true
+    url_path {
+      enable_id_contributor = true
+      service_id_contributor {
+        contribution_type = "OverrideValue"
+        value_override {
+          value = "Terraform"
+        }
+      }
+    }
+  }
+  insert_after = dynatrace_service_external_web_service.first-instance.id
 }
