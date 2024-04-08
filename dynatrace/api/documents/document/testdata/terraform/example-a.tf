3abdf70263a9808bc046037d47e0a7658199505c
Reinhard Pilz <reinhard.pilz@dynatrace.com>
2024-04-08 12:21:36 +0200

    Updated test data for resource `dynatrace_document`

--- dynatrace/api/documents/document/testdata/terraform/example-a.tf
+++ dynatrace/api/documents/document/testdata/terraform/example-a.tf
@@ -2,12 +2,6 @@ resource "dynatrace_document" "this" {
   type    = "dashboard"
   name    = "Example Dashboard"
   content = file(format("%s/example-dashboard.json", path.module))
-
-  lifecycle {
-    ignore_changes = [
-      owner
-    ]
-  }
 }
 
 data "dynatrace_documents" "all-dashboard-and-notebooks" {}
