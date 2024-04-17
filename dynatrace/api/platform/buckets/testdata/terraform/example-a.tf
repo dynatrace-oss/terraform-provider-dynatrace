e0ca9ddf7c7217faaa9e1a78a67a6f9e4805eeb0
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-04-17 15:18:55 -0400

    Fixed `dynatrace_platform_bucket` test case

--- dynatrace/api/platform/buckets/testdata/terraform/example-a.tf
+++ dynatrace/api/platform/buckets/testdata/terraform/example-a.tf
@@ -2,5 +2,5 @@ resource "dynatrace_platform_bucket" "#name#" {
   name         = "#name#"
   display_name = "Custom logs bucket playground"
   retention    = 67
-  table        = "spans"
+  table        = "logs"
 }
