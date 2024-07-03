f26d860418cd464696921907e36612cddf251e0a
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-07-03 13:48:56 -0400

    Updated resources with version update only

--- dynatrace/api/builtin/attribute/allowlist/service.go
+++ dynatrace/api/builtin/attribute/allowlist/service.go
@@ -23,7 +23,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.0.17"
+const SchemaVersion = "0.0.18"
 const SchemaID = "builtin:attribute-allow-list"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*allowlist.Settings] {
