6dda5ae56e43c547daea7c8bedc32b727e3421cf
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-07-30 14:34:24 -0400

    Updated `dynatrace_attack_rules` resource

--- dynatrace/api/builtin/appsec/attackprotectionadvancedconfig/settings/settings.go
+++ dynatrace/api/builtin/appsec/attackprotectionadvancedconfig/settings/settings.go
@@ -24,11 +24,13 @@ import (
 )
 
 type Settings struct {
-	AttackHandling *AttackHandling `json:"attackHandling"` // Step 2: Define attack control for chosen criteria
-	Criteria       *Criteria       `json:"criteria"`       // Step 1: Define criteria
-	Enabled        bool            `json:"enabled"`        // This setting is enabled (`true`) or disabled (`false`)
-	Metadata       *Metadata       `json:"metadata"`       // Step 3: Leave comment
-	InsertAfter    string          `json:"-"`
+	AttackHandling              *AttackHandling             `json:"attackHandling"`                        // Step 1: Select attack protection behavior
+	Criteria                    *Criteria                   `json:"criteria"`                              // Step 2: Select attack type
+	Enabled                     bool                        `json:"enabled"`                               // This setting is enabled (`true`) or disabled (`false`)
+	Metadata                    *Metadata                   `json:"metadata"`                              // Step 4: Leave comment (optional)
+	ResourceAttributeConditions ResourceAttributeConditions `json:"resourceAttributeConditions,omitempty"` // If you add more than one condition, note that all conditions must be true simultaneously for the rule to apply.\n\nWe provide suggestions for resource attribute keys and values based on what we currently see in your environment. You can also enter any value not currently seen in the list. Resource attributes come out of the box from the OneAgent, and you can set them up from [data enrichment](https://docs.dynatrace.com/docs/extend-dynatrace/extend-data).
+	RuleName                    *string                     `json:"ruleName,omitempty"`                    // Rule name
+	InsertAfter                 string                      `json:"-"`
 }
 
 func (me *Settings) Name() string {
@@ -39,7 +41,7 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
 		"attack_handling": {
 			Type:        schema.TypeList,
-			Description: "Step 2: Define attack control for chosen criteria",
+			Description: "Step 1: Select attack protection behavior",
 			Required:    true,
 			Elem:        &schema.Resource{Schema: new(AttackHandling).Schema()},
 			MinItems:    1,
@@ -47,7 +49,7 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		},
 		"criteria": {
 			Type:        schema.TypeList,
-			Description: "Step 1: Define criteria",
+			Description: "Step 2: Select attack type",
 			Required:    true,
 			Elem:        &schema.Resource{Schema: new(Criteria).Schema()},
 			MinItems:    1,
@@ -60,11 +62,24 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		},
 		"metadata": {
 			Type:        schema.TypeList,
-			Description: "Step 3: Leave comment",
+			Description: "Step 4: Leave comment (optional)",
 			Required:    true,
 			Elem:        &schema.Resource{Schema: new(Metadata).Schema()},
 			MinItems:    1,
 			MaxItems:    1,
+		},
+		"resource_attribute_conditions": {
+			Type:        schema.TypeList,
+			Description: "If you add more than one condition, note that all conditions must be true simultaneously for the rule to apply.\n\nWe provide suggestions for resource attribute keys and values based on what we currently see in your environment. You can also enter any value not currently seen in the list. Resource attributes come out of the box from the OneAgent, and you can set them up from [data enrichment](https://docs.dynatrace.com/docs/extend-dynatrace/extend-data).",
+			Optional:    true, // minobjects == 0
+			Elem:        &schema.Resource{Schema: new(ResourceAttributeConditions).Schema()},
+			MinItems:    1,
+			MaxItems:    1,
+		},
+		"rule_name": {
+			Type:        schema.TypeString,
+			Description: "Rule name",
+			Optional:    true, // nullable
 		},
 		"insert_after": {
 			Type:        schema.TypeString,
@@ -77,20 +92,24 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 
 func (me *Settings) MarshalHCL(properties hcl.Properties) error {
 	return properties.EncodeAll(map[string]any{
-		"attack_handling": me.AttackHandling,
-		"criteria":        me.Criteria,
-		"enabled":         me.Enabled,
-		"metadata":        me.Metadata,
-		"insert_after":    me.InsertAfter,
+		"attack_handling":               me.AttackHandling,
+		"criteria":                      me.Criteria,
+		"enabled":                       me.Enabled,
+		"metadata":                      me.Metadata,
+		"resource_attribute_conditions": me.ResourceAttributeConditions,
+		"rule_name":                     me.RuleName,
+		"insert_after":                  me.InsertAfter,
 	})
 }
 
 func (me *Settings) UnmarshalHCL(decoder hcl.Decoder) error {
 	return decoder.DecodeAll(map[string]any{
-		"attack_handling": &me.AttackHandling,
-		"criteria":        &me.Criteria,
-		"enabled":         &me.Enabled,
-		"metadata":        &me.Metadata,
-		"insert_after":    &me.InsertAfter,
+		"attack_handling":               &me.AttackHandling,
+		"criteria":                      &me.Criteria,
+		"enabled":                       &me.Enabled,
+		"metadata":                      &me.Metadata,
+		"resource_attribute_conditions": &me.ResourceAttributeConditions,
+		"rule_name":                     &me.RuleName,
+		"insert_after":                  &me.InsertAfter,
 	})
 }
