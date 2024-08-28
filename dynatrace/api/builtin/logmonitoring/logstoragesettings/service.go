534213af8352e140ba8c439733e8a960e1f8ba24
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-08-28 10:10:37 -0400

    Updated resources with version update only

--- dynatrace/api/builtin/logmonitoring/logstoragesettings/service.go
+++ dynatrace/api/builtin/logmonitoring/logstoragesettings/service.go
@@ -23,7 +23,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.10"
+const SchemaVersion = "1.0.11"
 const SchemaID = "builtin:logmonitoring.log-storage-settings"
 
 func Service(credentials *settings.Credentials) settings.CRUDService[*logstoragesettings.Settings] {
