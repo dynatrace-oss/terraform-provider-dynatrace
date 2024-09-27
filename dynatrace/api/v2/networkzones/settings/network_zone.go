49df5fb6ffc9ea519aa72ac20be81f14cb0e977d
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-09-27 14:00:09 -0400

    Resolved #540

--- dynatrace/api/v2/networkzones/settings/network_zone.go
+++ dynatrace/api/v2/networkzones/settings/network_zone.go
@@ -27,14 +27,15 @@ import (
 
 // NetworkZone TODO: documentation
 type NetworkZone struct {
-	ID                           *string  `json:"id,omitempty"` // The ID of the network zone
-	NetworkZoneName              *string  `json:"-"`
-	Description                  *string  `json:"description,omitempty"`                  // A short description of the network zone
-	AltZones                     []string `json:"alternativeZones,omitempty"`             // A list of alternative network zones.
-	NumOfOneAgentsFromOtherZones *int     `json:"numOfOneAgentsFromOtherZones,omitempty"` // The number of OneAgents from other network zones that are using ActiveGates in the network zone.
-	NumOfOneAgentsUsing          *int     `json:"numOfOneAgentsUsing,omitempty"`          // The number of OneAgents that are using ActiveGates in the network zone.
-	NumofConfiguredActiveGates   *int     `json:"numOfConfiguredActiveGates,omitempty"`   // The number of ActiveGates in the network zone.
-	NumOfConfiguredOneAgents     *int     `json:"numOfConfiguredOneAgents,omitempty"`     // The number of OneAgents that are configured to use the network zone as primary.
+	ID                           *string       `json:"id,omitempty"` // The ID of the network zone
+	NetworkZoneName              *string       `json:"-"`
+	Description                  *string       `json:"description,omitempty"`                  // A short description of the network zone
+	AltZones                     []string      `json:"alternativeZones,omitempty"`             // A list of alternative network zones.
+	FallbackMode                 *FallbackMode `json:"fallbackMode,omitempty"`                 // The fallback mode of the network zone. Possible values: `ANY_ACTIVE_GATE`, `NONE`, `ONLY_DEFAULT_ZONE`
+	NumOfOneAgentsFromOtherZones *int          `json:"numOfOneAgentsFromOtherZones,omitempty"` // The number of OneAgents from other network zones that are using ActiveGates in the network zone.
+	NumOfOneAgentsUsing          *int          `json:"numOfOneAgentsUsing,omitempty"`          // The number of OneAgents that are using ActiveGates in the network zone.
+	NumofConfiguredActiveGates   *int          `json:"numOfConfiguredActiveGates,omitempty"`   // The number of ActiveGates in the network zone.
+	NumOfConfiguredOneAgents     *int          `json:"numOfConfiguredOneAgents,omitempty"`     // The number of OneAgents that are configured to use the network zone as primary.
 }
 
 func (me *NetworkZone) Name() string {
@@ -62,6 +63,11 @@ func (me *NetworkZone) Schema() map[string]*schema.Schema {
 			Description: "A list of alternative network zones.",
 			Optional:    true,
 			Elem:        &schema.Schema{Type: schema.TypeString},
+		},
+		"fallback_mode": {
+			Type:        schema.TypeString,
+			Description: "The fallback mode of the network zone. Possible values: `ANY_ACTIVE_GATE`, `NONE`, `ONLY_DEFAULT_ZONE`",
+			Optional:    true,
 		},
 		"num_of_oneagents_from_other_zones": {
 			Type:             schema.TypeInt,
@@ -102,6 +108,7 @@ func (me *NetworkZone) MarshalHCL(properties hcl.Properties) error {
 		"name":                              me.ID,
 		"description":                       me.Description,
 		"alternative_zones":                 me.AltZones,
+		"fallback_mode":                     me.FallbackMode,
 		"num_of_oneagents_from_other_zones": me.NumOfOneAgentsFromOtherZones,
 		"num_of_oneagents_using":            me.NumOfOneAgentsUsing,
 		"num_of_configured_activegates":     me.NumofConfiguredActiveGates,
@@ -120,5 +127,6 @@ func (me *NetworkZone) UnmarshalHCL(decoder hcl.Decoder) error {
 		"name":              &me.NetworkZoneName,
 		"description":       &me.Description,
 		"alternative_zones": &me.AltZones,
+		"fallback_mode":     &me.FallbackMode,
 	})
 }
