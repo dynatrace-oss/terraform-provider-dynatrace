479160e753c8f872c0e387dc7d3819ec7fd34982
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2024-11-05 14:09:45 -0500

    Updated `dynatrace_log_sensitive_data_masking` resource

--- dynatrace/api/builtin/logmonitoring/sensitivedatamaskingsettings/settings/enums.go
+++ dynatrace/api/builtin/logmonitoring/sensitivedatamaskingsettings/settings/enums.go
@@ -39,6 +39,10 @@ var MatcherTypes = struct {
 	K8SContainerName       MatcherType
 	K8SDeploymentName      MatcherType
 	K8SNamespaceName       MatcherType
+	K8SPodAnnotation       MatcherType
+	K8SPodLabel            MatcherType
+	K8SWorkloadKind        MatcherType
+	K8SWorkloadName        MatcherType
 	LogSource              MatcherType
 	LogSourceOrigin        MatcherType
 	ProcessTechnology      MatcherType
@@ -50,6 +54,10 @@ var MatcherTypes = struct {
 	"k8s.container.name",
 	"k8s.deployment.name",
 	"k8s.namespace.name",
+	"k8s.pod.annotation",
+	"k8s.pod.label",
+	"k8s.workload.kind",
+	"k8s.workload.name",
 	"log.source",
 	"log.source.origin",
 	"process.technology",
