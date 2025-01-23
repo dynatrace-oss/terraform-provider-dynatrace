f83944bd95359cf05d15e55230f946ee3df676fa
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2025-01-23 16:48:19 -0500

    Updated `dynatrace_automation_workflow_aws_connections` test case

--- dynatrace/api/builtin/hyperscalerauthentication/awsconnection/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/hyperscalerauthentication/awsconnection/testdata/terraform/example_a.tf
@@ -2,6 +2,6 @@ resource "dynatrace_automation_workflow_aws_connections" "#name#" {
   name = "#name#"
   type = "webIdentity"
   web_identity {
-    role_arn    = "arn:aws:iam::helloworld:role.helloworld"
+    role_arn    = "arn:aws:iam::123456789012:role/helloworld"
   }
 }
