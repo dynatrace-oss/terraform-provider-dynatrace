31441518ad378d85864c76314ac6cb7fd8b297ce
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2025-04-25 10:17:07 -0400

    Updated resource with version update only

--- dynatrace/api/app/dynatrace/jiraconnection/service.go
+++ dynatrace/api/app/dynatrace/jiraconnection/service.go
@@ -24,7 +24,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.0.11"
+const SchemaVersion = "1"
 const SchemaID = "app:dynatrace.jira:connection"
 
 func Service(credentials *rest.Credentials) settings.CRUDService[*jiraconnection.Settings] {
