5fc7043e4ea03b9c8699a727865a4594060c10f8
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2025-05-13 12:54:26 -0400

    Updated resource with version update only

--- dynatrace/api/app/dynatrace/kubernetes/connector/connection/service.go
+++ dynatrace/api/app/dynatrace/kubernetes/connector/connection/service.go
@@ -24,7 +24,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.1.12"
+const SchemaVersion = "0.1.13"
 const SchemaID = "app:dynatrace.kubernetes.connector:connection"
 
 func Service(credentials *rest.Credentials) settings.CRUDService[*connection.Settings] {
