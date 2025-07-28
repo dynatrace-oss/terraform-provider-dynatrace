0bac767cdfda46952dc3e96ab2a0843a92c4a4b1
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2025-07-28 16:25:19 +0200

    feat: Adding dynatrace_github_connection to example

--- dynatrace/api/v2/settings/objects/permissions/testdata/terraform/example_a.tf
+++ dynatrace/api/v2/settings/objects/permissions/testdata/terraform/example_a.tf
@@ -10,6 +10,12 @@ data "dynatrace_iam_group" "example_group" {
   name = "Terraform Example"
 }
 
+resource "dynatrace_github_connection" "example_connection"{
+  name    = "GitHub connection"
+  type     = "pat"
+  token   = "azAZ09"
+}
+
 resource "dynatrace_setting_permissions" "github_connection_access" {
   settings_object_id = dynatrace_github_connection.example_connection.id
   all_users = "none"
