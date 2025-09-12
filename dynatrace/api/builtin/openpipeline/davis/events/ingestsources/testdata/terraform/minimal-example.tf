51a426ea30b2a26bffdffb90c479894f82e1d655
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2025-09-12 11:39:47 +0200

    feat(openpipeline): Copy-paste acceptance tests to all OpenPipeline data types

---
+++ dynatrace/api/builtin/openpipeline/davis/events/ingestsources/testdata/terraform/minimal-example.tf
@@ -0,0 +1,6 @@
+resource "dynatrace_openpipeline_v2_davis_events_ingestsources" "minimal-source" {
+  display_name = "min-ingest-source"
+  enabled = true
+  path_segment = "processor.ingestsource.path.tf.min.#name#"
+  processing {}
+}
