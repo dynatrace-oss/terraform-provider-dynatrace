b89ebdc593012bc440593504473adbfd52022c55
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2025-09-12 10:57:27 +0200

    feat(openpipeline): Removes routing table example with settings ID for now
    Currently this does not work due to the way repairInput is handled on server-side. Will re-add it later

--- dynatrace/api/builtin/openpipeline/events/routing/testdata/terraform/example.tf
+++ dynatrace/api/builtin/openpipeline/events/routing/testdata/terraform/example.tf
@@ -1,13 +1,6 @@
 resource "dynatrace_openpipeline_v2_events_routing" "routing" {
   routing_entries {
     routing_entry {
-      enabled       = true
-      pipeline_type = "custom"
-      pipeline_id   = dynatrace_openpipeline_v2_events_pipelines.routing-pipeline.id
-      matcher       = "matchesPhrase(record.title, \"Warning\")"
-      description   = "Warning route"
-    }
-    routing_entry {
       enabled             = true
       pipeline_type       = "builtin"
       builtin_pipeline_id = "default"
@@ -15,17 +8,4 @@ resource "dynatrace_openpipeline_v2_events_routing" "routing" {
       description         = "Default route"
     }
   }
-}
-
-resource "dynatrace_openpipeline_v2_events_pipelines" "routing-pipeline" {
-  display_name = "Routing pipeline"
-  custom_id = "pipeline_Routing_pipeline_1234_tf_#name#"
-  processing {}
-  davis {}
-  metric_extraction {}
-  security_context {}
-  cost_allocation {}
-  product_allocation {}
-  storage {}
-  data_extraction {}
 }
