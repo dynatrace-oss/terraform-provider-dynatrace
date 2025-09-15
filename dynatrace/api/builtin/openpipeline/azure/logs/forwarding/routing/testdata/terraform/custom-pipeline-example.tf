107b829e3048db3ba6afaa9875a7bfdeeea4fdea
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2025-09-15 14:38:24 +0200

    feat(openpipeline): Adds routing examples with custom pipelines

---
+++ dynatrace/api/builtin/openpipeline/azure/logs/forwarding/routing/testdata/terraform/custom-pipeline-example.tf
@@ -0,0 +1,24 @@
+resource "dynatrace_openpipeline_v2_azure_logs_forwarding_routing" "routing" {
+  routing_entries {
+    routing_entry {
+      enabled             = true
+      pipeline_type       = "custom"
+      pipeline_id = dynatrace_openpipeline_v2_azure_logs_forwarding_pipelines.pipeline.id
+      matcher             = "not matchesPhrase(record.title, \"Warning\")"
+      description         = "Default route"
+    }
+  }
+}
+
+resource "dynatrace_openpipeline_v2_azure_logs_forwarding_pipelines" "pipeline" {
+  display_name = "Minimal pipeline"
+  custom_id = "pipeline_Minimal_pipeline_1234_tf_#name#"
+  processing {}
+  davis {}
+  metric_extraction {}
+  security_context {}
+  cost_allocation {}
+  product_allocation {}
+  storage {}
+  data_extraction {}
+}
