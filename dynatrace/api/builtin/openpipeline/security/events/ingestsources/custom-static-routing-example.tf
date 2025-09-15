53e58a89ee4d4a89e4852a8f7ba048f1b1490d3a
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2025-09-15 14:26:21 +0200

    feat(openpipeline): Adds ingest-source examples with custom static routing

---
+++ dynatrace/api/builtin/openpipeline/security/events/ingestsources/custom-static-routing-example.tf
@@ -0,0 +1,24 @@
+resource "dynatrace_openpipeline_v2_security_events_ingestsources" "ingest-source" {
+  enabled = true
+  display_name = "ingest-source"
+  path_segment = "ingestsource.path.tf.#name#"
+  static_routing {
+    pipeline_type = "custom"
+    pipeline_id = dynatrace_openpipeline_v2_security_events_pipelines.pipeline.id
+  }
+  processing {
+  }
+}
+
+resource "dynatrace_openpipeline_v2_security_events_pipelines" "pipeline" {
+  display_name = "Pipeline"
+  custom_id = "pipeline_1234_tf_#name#"
+  processing {}
+  davis {}
+  metric_extraction {}
+  security_context {}
+  cost_allocation {}
+  product_allocation {}
+  storage {}
+  data_extraction {}
+}
