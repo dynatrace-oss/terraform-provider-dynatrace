49a6600740e9b39600b0b4ea6ed327ac97bee0b6
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-09-23 10:50:58 +0200

    feat(openpipeline): removed commented out HCL

--- dynatrace/api/builtin/openpipeline/davis/events/pipelines/testdata/terraform/maximal-example.tf
+++ dynatrace/api/builtin/openpipeline/davis/events/pipelines/testdata/terraform/maximal-example.tf
@@ -68,88 +68,9 @@ resource "dynatrace_openpipeline_v2_davis_events_pipelines" "max-pipeline" {
   }
   davis {
     # davis is not supported for davis.events pipelines
-    # processors {
-    #   processor {
-    #     type        = "davis"
-    #     id          = "processor_Create_warning_event_8226"
-    #     description = "Create warning event"
-    #     matcher     = "true"
-    #     davis {
-    #       properties {
-    #         property {
-    #           key = "event.type"
-    #           value = "CUSTOM_ALERT"
-    #         }
-    #         property {
-    #           key = "event.name"
-    #           value = "Warning detected"
-    #         }
-    #         property {
-    #           key = "event.description"
-    #           value = "Warning: {dims:record.summary}"
-    #         }
-    #       }
-    #     }
-    #     enabled = true
-    #   }
-    # }
   }
   metric_extraction {
     # metric_extraction is not supported for davis.events pipelines
-    # processors {
-    #   processor {
-    #     type        = "counterMetric"
-    #     id          = "processor_Count_warning_events_6392"
-    #     description = "Count warnings"
-    #     matcher     = "true"
-    #     counter_metric {
-    #       metric_key = "warning.count"
-    #       dimensions {
-    #         dimension {
-    #           source_field_name = "dt.cost.costcenter"
-    #         }
-    #         dimension {
-    #           source_field_name = "dt.cost.product"
-    #         }
-    #         dimension {
-    #           source_field_name = "dt.security_context"
-    #         }
-    #         dimension {
-    #           source_field_name      = "record.category"
-    #           destination_field_name = "warning_category"
-    #         }
-    #       }
-    #     }
-    #     enabled = true
-    #   }
-    #   processor {
-    #     type        = "valueMetric"
-    #     id          = "processor_Warning_timeout_1990"
-    #     description = "Warning timeout"
-    #     matcher     = "true"
-    #     value_metric {
-    #       metric_key    = "warning.timeout"
-    #       field         = "recording.timeout_in_min"
-    #       default_value = 60
-    #       dimensions {
-    #         dimension {
-    #           source_field_name = "dt.cost.costcenter"
-    #         }
-    #         dimension {
-    #           source_field_name = "dt.cost.product"
-    #         }
-    #         dimension {
-    #           source_field_name = "dt.security_context"
-    #         }
-    #         dimension {
-    #           source_field_name      = "record.category"
-    #           destination_field_name = "warning_category"
-    #         }
-    #       }
-    #     }
-    #     enabled = true
-    #   }
-    # }
   }
   security_context {
     processors {
