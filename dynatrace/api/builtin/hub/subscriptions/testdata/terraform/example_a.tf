a914b34be1ae831d5b57ba19f9c60a62849ffead
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-10-14 16:27:52 +0200

    test: fix flaky tests
    Removed some race conditions for tests. Some settings require a unique name, so tests in parallel may interfere if the name is static. Therefore, creating a unique name fixes the race condition.

--- dynatrace/api/builtin/hub/subscriptions/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/hub/subscriptions/testdata/terraform/example_a.tf
@@ -1,7 +1,7 @@
 resource "dynatrace_hub_subscriptions" "#name#" {
   token_subscriptions {
     token_subscription {
-      name        = "#name"
+      name        = "#name#"
       description = "Description"
       enabled     = true
       token       = "123456789012345678901234567890123456"
