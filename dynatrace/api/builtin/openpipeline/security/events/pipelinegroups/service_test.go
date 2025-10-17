d4f0f0c21ca8f9d41c6d5d90b3d7287809e2b7c9
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-10-17 13:56:57 +0200

    test: disable pipeline groups for tests
    Pipeline groups are not enabled yet. Therefore, disabling their tests

--- dynatrace/api/builtin/openpipeline/security/events/pipelinegroups/service_test.go
+++ dynatrace/api/builtin/openpipeline/security/events/pipelinegroups/service_test.go
@@ -4,8 +4,12 @@ import (
 	"testing"
 
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/testing/api"
+	featureflags "github.com/dynatrace-oss/terraform-provider-dynatrace/provider/featureflag"
 )
 
 func TestOpenPipelineSecurityEventsPipelineGroups(t *testing.T) {
+	if !featureflags.OpenPipelinePipelineGroups.Enabled() {
+		t.Skip("Pipeline groups are disabled")
+	}
 	api.TestAcc(t)
 }
