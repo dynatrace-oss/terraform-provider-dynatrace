52496c4ab6f0eaed1b7c29c5608841f79b3ace0f
Kodai Ishikawa <kodai.ishikawa@dynatrace.com>
2025-10-23 09:34:00 -0400

    Updated `dynatrace_davis_copilot` resource

--- dynatrace/api/service/daviscopilot/dataminingblocklist/settings/settings.go
+++ dynatrace/api/service/daviscopilot/dataminingblocklist/settings/settings.go
@@ -24,9 +24,10 @@ import (
 )
 
 type Settings struct {
-	BlocklistEntries            DataminingBlocklistEntries `json:"blocklistEntries,omitempty"`            // You can exclude specific data buckets and tables from the Davis CoPilot semantic index. Learn more about [configuring data access](https://dt-url.net/lc62i1q \"Davis CoPilot data access\").
-	EnableCopilot               bool                       `json:"enableCopilot"`                         // Please note that once enabled, you still need to [assign permissions](https://dt-url.net/rh22idn \"Davis CoPilot permissions\") to the relevant user groups.
-	EnableTenantAwareDataMining *bool                      `json:"enableTenantAwareDataMining,omitempty"` // You can enrich Davis CoPilot with your environment data. This lets you generate more accurate queries that identify and reference relevant entities, events, spans, logs, and metrics from your environment. Once enabled, Davis CoPilot periodically scans your Grail data to create its own semantic index. Please note, it can take up to 24 hours to reflect changes. Learn more about [environment-aware queries](https://dt-url.net/4g42iu7 \"Davis CoPilot environment aware queries\").
+	BlocklistEntries            DataminingBlocklistEntries `json:"blocklistEntries,omitempty"`            // You can exclude specific data buckets and tables from the semantic index. Learn more about [configuring data access](https://dt-url.net/lc62i1q \"Dynatrace Generative AI data access\").
+	EnableCopilot               bool                       `json:"enableCopilot"`                         // Please note that once enabled, you still need to [assign permissions](https://dt-url.net/rh22idn \"Dynatrace Generative AI permissions\") to the relevant user groups.
+	EnableDocumentSuggestion    *bool                      `json:"enableDocumentSuggestion,omitempty"`    // By enabling document suggestions, Dynatrace AI can find similarities between Problems and existing Notebooks and Dashboards in order to suggest relevant troubleshooting guides. Learn more about [document suggestions](https://dt-url.net/xy02gpo \"Dynatrace AI document suggestions\").
+	EnableTenantAwareDataMining *bool                      `json:"enableTenantAwareDataMining,omitempty"` // You can enrich Dynatrace Generative AI with your environment data. This lets you generate more accurate queries that identify and reference relevant entities, events, spans, logs, and metrics from your environment. Once enabled, Dynatrace AI periodically scans your Grail data to create its own semantic index. Please note, it can take up to 24 hours to reflect changes. Learn more about [environment-aware queries](https://dt-url.net/4g42iu7 \"Dynatrace Generative AI environment aware queries\").
 }
 
 func (me *Settings) Name() string {
@@ -37,7 +38,7 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
 		"blocklist_entries": {
 			Type:        schema.TypeList,
-			Description: "You can exclude specific data buckets and tables from the Davis CoPilot semantic index. Learn more about [configuring data access](https://dt-url.net/lc62i1q \"Davis CoPilot data access\").",
+			Description: "You can exclude specific data buckets and tables from the semantic index. Learn more about [configuring data access](https://dt-url.net/lc62i1q \"Dynatrace Generative AI data access\").",
 			Optional:    true, // precondition & minobjects == 0
 			Elem:        &schema.Resource{Schema: new(DataminingBlocklistEntries).Schema()},
 			MinItems:    1,
@@ -45,12 +46,17 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		},
 		"enable_copilot": {
 			Type:        schema.TypeBool,
-			Description: "Please note that once enabled, you still need to [assign permissions](https://dt-url.net/rh22idn \"Davis CoPilot permissions\") to the relevant user groups.",
+			Description: "Please note that once enabled, you still need to [assign permissions](https://dt-url.net/rh22idn \"Dynatrace Generative AI permissions\") to the relevant user groups.",
 			Required:    true,
+		},
+		"enable_document_suggestion": {
+			Type:        schema.TypeBool,
+			Description: "By enabling document suggestions, Dynatrace AI can find similarities between Problems and existing Notebooks and Dashboards in order to suggest relevant troubleshooting guides. Learn more about [document suggestions](https://dt-url.net/xy02gpo \"Dynatrace AI document suggestions\").",
+			Optional:    true, // precondition
 		},
 		"enable_tenant_aware_data_mining": {
 			Type:        schema.TypeBool,
-			Description: "You can enrich Davis CoPilot with your environment data. This lets you generate more accurate queries that identify and reference relevant entities, events, spans, logs, and metrics from your environment. Once enabled, Davis CoPilot periodically scans your Grail data to create its own semantic index. Please note, it can take up to 24 hours to reflect changes. Learn more about [environment-aware queries](https://dt-url.net/4g42iu7 \"Davis CoPilot environment aware queries\").",
+			Description: "You can enrich Dynatrace Generative AI with your environment data. This lets you generate more accurate queries that identify and reference relevant entities, events, spans, logs, and metrics from your environment. Once enabled, Dynatrace AI periodically scans your Grail data to create its own semantic index. Please note, it can take up to 24 hours to reflect changes. Learn more about [environment-aware queries](https://dt-url.net/4g42iu7 \"Dynatrace Generative AI environment aware queries\").",
 			Optional:    true, // precondition
 		},
 	}
@@ -60,11 +66,15 @@ func (me *Settings) MarshalHCL(properties hcl.Properties) error {
 	return properties.EncodeAll(map[string]any{
 		"blocklist_entries":               me.BlocklistEntries,
 		"enable_copilot":                  me.EnableCopilot,
+		"enable_document_suggestion":      me.EnableDocumentSuggestion,
 		"enable_tenant_aware_data_mining": me.EnableTenantAwareDataMining,
 	})
 }
 
 func (me *Settings) HandlePreconditions() error {
+	if (me.EnableDocumentSuggestion == nil) && (me.EnableCopilot) {
+		me.EnableDocumentSuggestion = opt.NewBool(false)
+	}
 	if (me.EnableTenantAwareDataMining == nil) && (me.EnableCopilot) {
 		me.EnableTenantAwareDataMining = opt.NewBool(false)
 	}
@@ -76,6 +86,7 @@ func (me *Settings) UnmarshalHCL(decoder hcl.Decoder) error {
 	return decoder.DecodeAll(map[string]any{
 		"blocklist_entries":               &me.BlocklistEntries,
 		"enable_copilot":                  &me.EnableCopilot,
+		"enable_document_suggestion":      &me.EnableDocumentSuggestion,
 		"enable_tenant_aware_data_mining": &me.EnableTenantAwareDataMining,
 	})
 }
