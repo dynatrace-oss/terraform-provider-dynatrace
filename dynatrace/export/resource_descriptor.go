1a91df120c0240d573af9dfa0209e436c89bc511
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-10-27 16:09:05 +0100

    feat(openpipeline-v2): wire up pipeline-group resources
    This adds the pipeline groups to the provider and comments it out

--- dynatrace/export/resource_descriptor.go
+++ dynatrace/export/resource_descriptor.go
@@ -428,6 +428,19 @@ import (
 	openpipelineusersessionsingestsources "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/usersessions/ingestsources"
 	openpipelineusersessionspipelines "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/usersessions/pipelines"
 	openpipelineusersessionsrouting "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/usersessions/routing"
+	//openpipelinebizeventspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/bizevents/pipelinegroups"
+	//openpipelinedaviseventspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/davis/events/pipelinegroups"
+	//openpipelinedavisproblemspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/davis/problems/pipelinegroups"
+	//openpipelineeventspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/events/pipelinegroups"
+	//openpipelineeventssdlcpipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/events/sdlc/pipelinegroups"
+	//openpipelineeventssecuritypipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/events/security/pipelinegroups"
+	//openpipelinelogspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/logs/pipelinegroups"
+	//openpipelinemetricspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/metrics/pipelinegroups"
+	//openpipelinesecurityeventspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/security/events/pipelinegroups"
+	//openpipelinespanspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/spans/pipelinegroups"
+	//openpipelinesystemeventspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/system/events/pipelinegroups"
+	//openpipelineusereventspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/user/events/pipelinegroups"
+	//openpipelineusersessionspipelinegroups "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/openpipeline/usersessions/pipelinegroups"
 )
 
 func NewResourceDescriptor[T settings.Settings](fn func(credentials *rest.Credentials) settings.CRUDService[T], dependencies ...Dependency) ResourceDescriptor {
@@ -1770,6 +1783,19 @@ var AllResources = map[ResourceType]ResourceDescriptor{
 		Dependencies.ID(ResourceTypes.OpenpipelineUsersessionsPipelines),
 	),
 	ResourceTypes.AutomationApproval: NewResourceDescriptor(approval.Service),
+	//ResourceTypes.OpenpipelineBizeventsPipelinegroups:      NewResourceDescriptor(openpipelinebizeventspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineDavisEventsPipelinegroups:    NewResourceDescriptor(openpipelinedaviseventspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineDavisProblemsPipelinegroups:  NewResourceDescriptor(openpipelinedavisproblemspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineEventsPipelinegroups:         NewResourceDescriptor(openpipelineeventspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineEventsSdlcPipelinegroups:     NewResourceDescriptor(openpipelineeventssdlcpipelinegroups.Service),
+	//ResourceTypes.OpenpipelineEventsSecurityPipelinegroups: NewResourceDescriptor(openpipelineeventssecuritypipelinegroups.Service),
+	//ResourceTypes.OpenpipelineLogsPipelinegroups:           NewResourceDescriptor(openpipelinelogspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineMetricsPipelinegroups:        NewResourceDescriptor(openpipelinemetricspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineSecurityEventsPipelinegroups: NewResourceDescriptor(openpipelinesecurityeventspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineSpansPipelinegroups:          NewResourceDescriptor(openpipelinespanspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineSystemEventsPipelinegroups:   NewResourceDescriptor(openpipelinesystemeventspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineUserEventsPipelinegroups:     NewResourceDescriptor(openpipelineusereventspipelinegroups.Service),
+	//ResourceTypes.OpenpipelineUsersessionsPipelinegroups:   NewResourceDescriptor(openpipelineusersessionspipelinegroups.Service),
 }
 
 type ResourceExclusion struct {
