67cc3f743d643e15800406e433de41d4e5bef3ce
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-11-17 15:53:54 +0100

    test: fix extension test
    The API returns two default values now, which breaks the test (non-empty plan)
    Adding the default values to the config solves the problem.

--- dynatrace/api/v2/hub/extension/config/testdata/terraform/example-a.tf
+++ dynatrace/api/v2/hub/extension/config/testdata/terraform/example-a.tf
@@ -2,6 +2,8 @@ resource "dynatrace_hub_extension_config" "com_dynatrace_extension_jmx-weblogic-
   name = "com.dynatrace.extension.jmx-weblogic-cp"
     value = jsonencode(
     {
+      "activationContext": "LOCAL",
+      "activationTags": [],
       "enabled" : true,
       "description" : "jj",
       "version" : "2.0.4",
