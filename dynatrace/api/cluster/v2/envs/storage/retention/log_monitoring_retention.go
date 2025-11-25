dfaf6d0484234095dc6ec8351239105496a7bded
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2025-11-25 08:46:28 +0100

    chore: add Apache 2.0 license headers to Go source files
    Added standardized Apache License, Version 2.0 headers to all Go source files across datasources, Dynatrace API, export, rest, settings, provider, and tools directories to ensure proper licensing and copyright attribution for 2025 Dynatrace LLC.

--- dynatrace/api/cluster/v2/envs/storage/retention/log_monitoring_retention.go
+++ dynatrace/api/cluster/v2/envs/storage/retention/log_monitoring_retention.go
@@ -1,3 +1,20 @@
+/**
+* @license
+* Copyright 2025 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
 package retention
 
 // LogMonitoring represents log monitoring retention settings on environment level. Not editable when Log monitoring is not allowed by license or not configured on cluster level. Can be set to any value from 5 to 90 days. If skipped when editing via PUT method then already set limit will remain
--- dynatrace/api/cluster/v2/envs/storage/retention/service_code_level.go
+++ dynatrace/api/cluster/v2/envs/storage/retention/service_code_level.go
@@ -1,3 +1,20 @@
+/**
+* @license
+* Copyright 2025 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
 package retention
 
 // ServiceCodeLevel represents service code level retention settings on environment level. Service code level retention time can't be greater than service request level retention time and both can't exceed one year.If skipped when editing via PUT method then already set limit will remain
