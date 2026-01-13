dc351cfd206d9979cebbee846bf4addfb2e76d08
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-01-13 12:17:27 +0100

    feat: update dynatrace_azure_connection and dynatrace_azure_connection_authentication to 0.0.11

--- dynatrace/api/builtin/hyperscalerauthentication/connections/azure/service.go
+++ dynatrace/api/builtin/hyperscalerauthentication/connections/azure/service.go
@@ -31,7 +31,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.0.10"
+const SchemaVersion = "0.0.11"
 const SchemaID = "builtin:hyperscaler-authentication.connections.azure"
 const DefaultTimeout = 2 * time.Minute
 
