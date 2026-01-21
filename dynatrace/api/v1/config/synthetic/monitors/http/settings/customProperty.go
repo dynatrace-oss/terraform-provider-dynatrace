f14a792e98677d515bf565bf19d6c4a4d00cef45
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-01-21 18:36:23 +0100

    feat: support advanced monitor settings for `dynatrace_http_monitor`
    This introduces the preview only, advanced settings (custom properties) for synthetic monitors (v1)

---
+++ dynatrace/api/v1/config/synthetic/monitors/http/settings/customProperty.go
@@ -0,0 +1,90 @@
+/*
+ * @license
+ * Copyright 2026 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package http
+
+import (
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/terraform/hcl"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type CustomProperties []*CustomProperty
+
+func (me *CustomProperties) Schema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"custom_property": {
+			Type:        schema.TypeSet,
+			Description: "Custom properties for the monitor",
+			Required:    true,
+			MinItems:    1,
+			Elem:        &schema.Resource{Schema: new(CustomProperty).Schema()},
+		},
+	}
+}
+
+func (me CustomProperties) MarshalHCL(properties hcl.Properties) error {
+	return properties.EncodeSlice("custom_property", me)
+}
+
+func (me *CustomProperties) UnmarshalHCL(decoder hcl.Decoder) error {
+	if err := decoder.DecodeSlice("custom_property", me); err != nil {
+		return err
+	}
+	// https://github.com/hashicorp/terraform-plugin-sdk/issues/895
+	// Only known workaround is to ignore these blocks
+	customProperties := CustomProperties{}
+	for _, property := range *me {
+		if property.Name != "" || property.Value != "" {
+			customProperties = append(customProperties, property)
+		}
+	}
+	*me = customProperties
+	return nil
+}
+
+type CustomProperty struct {
+	Name  string `json:"name"`  // The name of the custom property
+	Value string `json:"value"` // The value of the custom property
+}
+
+func (me *CustomProperty) Schema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"name": {
+			Type:        schema.TypeString,
+			Description: "The name of the custom property. Possible values: `hmRequestTimeoutInMs`, `hmConnectTimeoutInMs`, `hmMaxHeaderSizeInBytes`, `hmMonitorExecutionTimeoutInMs`, `hmScriptExecutionTimeoutInMs`, `hmMaxRequestBodySizeInBytes`, `hmMaxCustomScriptSizeInBytes`, `hmMaxResponseBodySizeInBytes`, `hmMaxResponseBodySizeToCustomScriptInBytes`, `hmDnsQueryTimeoutInMs`",
+			Required:    true,
+		},
+		"value": {
+			Type:        schema.TypeString,
+			Description: "The value of the custom property",
+			Required:    true,
+		},
+	}
+}
+
+func (me *CustomProperty) MarshalHCL(properties hcl.Properties) error {
+	return properties.EncodeAll(map[string]any{
+		"name":  me.Name,
+		"value": me.Value,
+	})
+}
+
+func (me *CustomProperty) UnmarshalHCL(decoder hcl.Decoder) error {
+	return decoder.DecodeAll(map[string]any{
+		"name":  &me.Name,
+		"value": &me.Value,
+	})
+}
---
+++ dynatrace/api/v1/config/synthetic/monitors/http/testdata/terraform/example_http_advanced.tf
@@ -0,0 +1,68 @@
+resource "dynatrace_http_monitor" "advanced" {
+  name      = "#name#"
+  frequency = 1
+  locations = ["GEOLOCATION-F3E06A526BE3B4C4"]
+  anomaly_detection {
+    loading_time_thresholds {
+    }
+    outage_handling {
+      global_outage = true
+      global_outage_policy {
+        consecutive_runs = 1
+      }
+    }
+  }
+  script {
+    request {
+      description     = "getOffice365ActiveUserCounts"
+      method          = "GET"
+      url             = "https://graph.microsoft.com/v1.0/reports/getOffice365ActiveUserCounts(period='D7')"
+      configuration {
+        accept_any_certificate = true
+        follow_redirects       = true
+      }
+    }
+    custom_properties {
+      custom_property {
+        name = "hmRequestTimeoutInMs"
+        value = "5000"
+      }
+      custom_property {
+        name = "hmConnectTimeoutInMs"
+        value = "5000"
+      }
+      custom_property {
+        name = "hmMaxHeaderSizeInBytes"
+        value = "10240"
+      }
+      custom_property {
+        name = "hmMonitorExecutionTimeoutInMs"
+        value = "10000"
+      }
+      custom_property {
+        name = "hmScriptExecutionTimeoutInMs"
+        value = "5000"
+      }
+      custom_property {
+        name = "hmMaxRequestBodySizeInBytes"
+        value = "10240"
+      }
+      custom_property {
+        name = "hmMaxCustomScriptSizeInBytes"
+        value = "10240"
+      }
+      custom_property {
+        name = "hmMaxResponseBodySizeInBytes"
+        value = "51200"
+      }
+      custom_property {
+        name = "hmMaxResponseBodySizeToCustomScriptInBytes"
+        value = "10240"
+      }
+      custom_property {
+        name = "hmDnsQueryTimeoutInMs"
+        value = "5000"
+      }
+    }
+  }
+}
