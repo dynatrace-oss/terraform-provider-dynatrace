b4032a755c7d2a60469521d2085d862fa0261622
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-01-22 15:15:01 +0100

    refactor: use core client for iam client
    This uses the core client instead of the custom client, which includes correct OAuth token exchange handling and different retry mechanisms

--- dynatrace/api/iam/iam_client.go
+++ dynatrace/api/iam/iam_client.go
@@ -21,21 +21,25 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
-	"log"
-	"math"
 	"net/http"
+	"net/url"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest/logging"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/provider/version"
 	"github.com/google/uuid"
+	"golang.org/x/oauth2/clientcredentials"
+
+	"github.com/dynatrace/dynatrace-configuration-as-code-core/api/auth"
+	rest2 "github.com/dynatrace/dynatrace-configuration-as-code-core/api/rest"
 )
 
 type IAMError struct {
@@ -58,33 +62,32 @@ type IAMClient interface {
 }
 
 type iamClient struct {
-	auth Authenticator
+	auth   Authenticator
+	client *rest2.Client
 }
 
-func NewIAMClient(auth Authenticator) IAMClient {
-	return &iamClient{auth}
-}
-
-func (me *iamClient) authenticate(ctx context.Context, httpRequest *http.Request, forceNew bool) error {
-	var bearerToken string
-	var err error
-
-	if bearerToken, err = getBearer(ctx, me.auth, forceNew); err != nil {
-		return err
+func NewIAMClient(a Authenticator) IAMClient {
+	oauthConfig := clientcredentials.Config{
+		ClientID:     a.ClientID(),
+		ClientSecret: a.ClientSecret(),
+		TokenURL:     a.TokenURL(),
 	}
+	httpClient := auth.NewOAuthClient(context.Background(), &oauthConfig)
 
-	if os.Getenv("DT_DEBUG_IAM_BEARER") == "true" {
-		log.Println("--- BEARER TOKEN ---")
-		log.Println(bearerToken)
-		log.Println("--------------------")
+	opts := []rest2.Option{
+		rest2.WithHTTPListener(logging.HTTPListener("iam")),
+		rest2.WithRateLimiter(),
+		rest2.WithRetryOptions(&rest2.RetryOptions{
+			DelayAfterRetry: time.Second * 60,
+			MaxRetries:      10,
+			ShouldRetryFunc: func(resp *http.Response) bool {
+				return resp.StatusCode == 429 || resp.StatusCode == 504
+			},
+		}),
 	}
-	if len(strings.TrimSpace(bearerToken)) == 0 {
-		return errors.New(msgInvalidOAuthCredentials)
-	}
-
-	httpRequest.Header.Set("Authorization", "Bearer "+bearerToken)
-
-	return nil
+	eUrl, _ := url.Parse(a.EndpointURL())
+	client := rest2.NewClient(eUrl, httpClient, opts...)
+	return &iamClient{a, client}
 }
 
 func (me *iamClient) POST(ctx context.Context, url string, payload any, expectedResponseCode int, forceNewBearer bool) ([]byte, error) {
@@ -169,104 +172,52 @@ func (me *iamClient) request(ctx context.Context, url string, method string, exp
 	}
 }
 
-func (me *iamClient) _request(ctx context.Context, url string, method string, expectedResponseCodes []int, forceNewBearer bool, forceNewBearerRetryCount int, payload any, headers map[string]string) ([]byte, error) {
-	// httplog(fmt.Sprintf("[%s] %s", method, url))
-
+func (me *iamClient) _request(ctx context.Context, u string, method string, expectedResponseCodes []int, forceNewBearer bool, forceNewBearerRetryCount int, payload any, headers map[string]string) ([]byte, error) {
 	id := uuid.NewString()
-	num504Retries := 0
-	sleepTime429 := int64(500)
 
-	for {
-		var err error
-		var httpRequest *http.Request
-		var httpResponse *http.Response
-		var responseBytes []byte
-		var requestBody []byte
+	var err error
+	var httpRequest *http.Request
+	var httpResponse *http.Response
+	var responseBytes []byte
+	var requestBody []byte
 
-		rest.Logger.Printf(ctx, "[%s] %s %s", id, method, url)
+	rest.Logger.Printf(ctx, "[%s] %s %s", id, method, u)
 
-		if requestBody, err = json.Marshal(payload); err != nil {
-			return nil, err
-		}
-		if payload != nil {
-			rest.Logger.Printf(ctx, "[%s] [PAYLOAD] %s", id, string(requestBody))
-		}
+	if requestBody, err = json.Marshal(payload); err != nil {
+		return nil, err
+	}
+	if payload != nil {
+		rest.Logger.Printf(ctx, "[%s] [PAYLOAD] %s", id, string(requestBody))
+	}
 
-		var body io.Reader
+	var body io.Reader
 
-		if payload != nil {
-			body = bytes.NewReader(requestBody)
-		}
+	if payload != nil {
+		body = bytes.NewReader(requestBody)
+	}
 
-		if httpRequest, err = http.NewRequest(method, url, body); err != nil {
-			return nil, err
-		}
+	if httpRequest, err = http.NewRequest(method, u, body); err != nil {
+		return nil, err
+	}
 
-		if err = me.authenticate(ctx, httpRequest, forceNewBearer); err != nil {
-			return nil, err
-		}
+	for k, v := range headers {
+		httpRequest.Header.Add(k, v)
+	}
 
-		for k, v := range headers {
-			httpRequest.Header.Add(k, v)
-		}
+	httpRequest.Header.Set("User-Agent", version.UserAgent())
 
-		httpRequest.Header.Set("User-Agent", version.UserAgent())
+	if httpResponse, err = me.client.Do(httpRequest); err != nil {
+		return nil, err
+	}
 
-		if httpResponse, err = http.DefaultClient.Do(httpRequest); err != nil {
-			return nil, err
-		}
+	if responseBytes, err = io.ReadAll(httpResponse.Body); err != nil {
+		return nil, err
+	}
+	rest.Logger.Printf(ctx, "[%s] [RESPONSE] %d %s", id, httpResponse.StatusCode, string(responseBytes))
 
-		if responseBytes, err = io.ReadAll(httpResponse.Body); err != nil {
-			return nil, err
-		}
-		rest.Logger.Printf(ctx, "[%s] [RESPONSE] %d %s", id, httpResponse.StatusCode, string(responseBytes))
-
-		if httpResponse.StatusCode == 504 {
-			// httplog("-------------------- FIVE-O-FOUR --------------------")
-			num504Retries++
-			if num504Retries > 5 {
-				return nil, fmt.Errorf("response code %d (expected: %d)", 504, expectedResponseCodes)
-			}
-			time.Sleep(time.Second)
-			continue
-		}
-
-		isNotExpectedResponseCode := true
-		for _, erc := range expectedResponseCodes {
-			if httpResponse.StatusCode == erc {
-				isNotExpectedResponseCode = false
-				break
-			}
-		}
-
-		if isNotExpectedResponseCode {
-			if httpResponse.StatusCode == 429 {
-				time.Sleep(time.Duration(sleepTime429) * time.Millisecond)
-				// logging.File.Println(".... 429 ... waiting for another", sleepTime429, "milliseconds")
-				sleepTime429 = int64(math.Round(float64(sleepTime429) * float64(1.6)))
-				continue
-			}
-			var iamErr IAMError
-			if err = json.Unmarshal(responseBytes, &iamErr); err == nil {
-				if (forceNewBearerRetryCount < 20) && iamErr.Error() == "Failed to validate access token." {
-					// httplog("-------------------- TOKEN-SWITCH --------------------")
-					return me.request(ctx, url, method, expectedResponseCodes, true, forceNewBearerRetryCount+1, payload, headers)
-				}
-				return nil, iamErr
-			} else {
-				errEnv := struct {
-					Error *rest.Error `json:"error"`
-				}{}
-				if len(responseBytes) > 0 {
-					if err = json.Unmarshal(responseBytes, &errEnv); err == nil && errEnv.Error != nil {
-						// {"error":{"code":400,"message":"Policy c1f6bc44-c66d-4964-938c-ab00823a5e22 can't be bound to levelType: environment, levelId: siz65484","errorsMap":null}}
-						return nil, *errEnv.Error
-					}
-				}
-				return nil, rest.Error{Code: httpResponse.StatusCode, Message: fmt.Sprintf("response code %d (expected: %d)", httpResponse.StatusCode, expectedResponseCodes)}
-			}
-		}
-
+	if slices.Contains(expectedResponseCodes, httpResponse.StatusCode) {
 		return responseBytes, nil
 	}
+
+	return nil, rest.Error{Code: httpResponse.StatusCode, Message: fmt.Sprintf("response code %d (expected: %d)", httpResponse.StatusCode, expectedResponseCodes)}
 }
