7f2bc2756c54ca8672bf3f761b4d0a5da769217d
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2026-01-26 15:13:46 +0100

    test(boundaries): Adds dynatrace_iam_policy_boundary E2E test

---
+++ dynatrace/api/iam/boundaries/service_test.go
@@ -0,0 +1,58 @@
+//go:build integration
+
+/*
+ * @license
+ * Copyright 2026 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boundaries_test
+
+import (
+	"testing"
+
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/testing/api"
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/provider"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func TestAccBoundaries(t *testing.T) {
+	if !api.AccEnvsGiven(t) {
+		return
+	}
+
+	boundaryName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	t.Setenv("TF_VAR_BOUNDARY_NAME", boundaryName)
+
+	configCreate, _ := api.ReadTfConfig(t, "testdata/create.tf")
+	configUpdate, _ := api.ReadTfConfig(t, "testdata/update.tf")
+
+	providerFactories := map[string]func() (*schema.Provider, error){
+		"dynatrace": func() (*schema.Provider, error) {
+			return provider.Provider(), nil
+		},
+	}
+
+	t.Run("Create boundary and update it", func(t *testing.T) {
+		testCase := resource.TestCase{
+			ProviderFactories: providerFactories,
+			Steps: []resource.TestStep{
+				{Config: configCreate}, // creates boundary
+				{Config: configUpdate}, // Updates boundary
+			},
+		}
+		resource.Test(t, testCase)
+	})
+}
