25cf3dc28233dd67c6972a9824f7cabacdc68453
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-01-28 17:26:05 +0100

    feat: update dynatrace_log_timestamp to 1.0.19

--- dynatrace/api/builtin/logmonitoring/timestampconfiguration/service.go
+++ dynatrace/api/builtin/logmonitoring/timestampconfiguration/service.go
@@ -18,15 +18,15 @@
 package timestampconfiguration
 
 import (
-	timestampconfiguration "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/logmonitoring/timestampconfiguration/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/logmonitoring/timestampconfiguration/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.18"
+const SchemaVersion = "1.0.19"
 const SchemaID = "builtin:logmonitoring.timestamp-configuration"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*timestampconfiguration.Settings] {
-	return settings20.Service[*timestampconfiguration.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
