303d6bf38fcccffb5412b9947d32659eac5fcc07
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-02-04 15:10:53 +0100

    test: remove static request attribute and management zone dependency
    Removes the static request attribute and management zone dependency for `dynatrace_calculated_service_metric` and creates them on demand

--- dynatrace/api/v1/config/metrics/calculated/service/testdata/terraform/example_d.tf
+++ dynatrace/api/v1/config/metrics/calculated/service/testdata/terraform/example_d.tf
@@ -1,7 +1,8 @@
-resource "dynatrace_calculated_service_metric" "#name#" {
+resource "dynatrace_calculated_service_metric" "metric_d" {
+  depends_on = [time_sleep.wait_for_request_attributes]
   name             = "#name#"
   enabled          = true
-  management_zones = ["AAAA"]
+  management_zones = [dynatrace_management_zone_v2.mzone.name]
   metric_key       = "calc:service.#name#"
   unit             = "MILLI_SECOND_PER_MINUTE"
   conditions {
@@ -19,6 +20,6 @@ resource "dynatrace_calculated_service_metric" "#name#" {
   }
   metric_definition {
     metric            = "REQUEST_ATTRIBUTE"
-    request_attribute = "foo"
+    request_attribute = dynatrace_request_attribute.attribute.name
   }
 }
