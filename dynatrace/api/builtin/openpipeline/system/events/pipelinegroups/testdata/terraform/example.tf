261af3123cd3ec72716294e4a756d007d5db9f99
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-02-10 16:37:46 +0100

    test(openpipeline-v2): add tests for pipeline groups
    This adds the E2E tests for the pipeline groups. The examples are mostly identical.
    It's two pipelines, an empty one and one with davis/metricExtractions/securityContext defined, depending on whether it's enabled on that resource.
    One pipeline group that creates a group with the stages of a pipeline (the second-mentioned one) and assigns it to another pipeline (the first-mentioned one)

---
+++ dynatrace/api/builtin/openpipeline/system/events/pipelinegroups/testdata/terraform/example.tf
@@ -0,0 +1,118 @@
+resource "dynatrace_openpipeline_v2_system_events_pipelines" "example1" {
+  display_name = "#name#"
+  custom_id = "#name#"
+  group_role = "compositionPipeline"
+  routing = "notRoutable"
+  davis {
+    processors {
+      processor {
+        type        = "davis"
+        id          = "processor_Create_warning_event_8226"
+        description = "Create warning event"
+        matcher     = "true"
+        davis {
+          properties {
+            property {
+              key = "event.type"
+              value = "CUSTOM_ALERT"
+            }
+            property {
+              key = "event.name"
+              value = "Warning detected"
+            }
+            property {
+              key = "event.description"
+              value = "Warning: {dims:record.summary}"
+            }
+          }
+        }
+        enabled = true
+      }
+    }
+  }
+  metric_extraction {
+    processors {
+      processor {
+        type        = "counterMetric"
+        id          = "processor_Count_warning_events_6392"
+        description = "Count warnings"
+        matcher     = "true"
+        counter_metric {
+          metric_key = "warning.count"
+          dimensions {
+            dimension {
+              source_field_name = "dt.cost.costcenter"
+            }
+            dimension {
+              source_field_name = "dt.cost.product"
+            }
+            dimension {
+              source_field_name = "dt.security_context"
+            }
+            dimension {
+              source_field_name      = "record.category"
+              destination_field_name = "warning_category"
+            }
+          }
+        }
+        enabled = true
+      }
+      processor {
+        type        = "valueMetric"
+        id          = "processor_Warning_timeout_1990"
+        description = "Warning timeout"
+        matcher     = "true"
+        value_metric {
+          metric_key    = "warning.timeout"
+          field         = "recording.timeout_in_min"
+          default_value = 60
+          dimensions {
+            dimension {
+              source_field_name = "dt.cost.costcenter"
+            }
+            dimension {
+              source_field_name = "dt.cost.product"
+            }
+            dimension {
+              source_field_name = "dt.security_context"
+            }
+            dimension {
+              source_field_name      = "record.category"
+              destination_field_name = "warning_category"
+            }
+          }
+        }
+        enabled = true
+      }
+    }
+  }
+}
+
+
+resource "dynatrace_openpipeline_v2_system_events_pipelines" "example2" {
+  display_name = "#name#-2"
+  custom_id = "#name#-2"
+  group_role = "memberPipeline"
+}
+
+resource "dynatrace_openpipeline_v2_system_events_pipelinegroups" "example" {
+  display_name = "#name#"
+  composition {
+    pipeline_group_composition {
+      is_pipeline_placeholder = true
+    }
+    pipeline_group_composition {
+      is_pipeline_placeholder = false
+      stages {
+        type = "include"
+        include = ["davis", "metricExtraction"]
+      }
+      pipeline_id = dynatrace_openpipeline_v2_system_events_pipelines.example1.id
+    }
+  }
+  member_stages {
+    include = ["davis", "metricExtraction"]
+    type = "include"
+  }
+  member_pipelines = [dynatrace_openpipeline_v2_system_events_pipelines.example2.id]
+}
