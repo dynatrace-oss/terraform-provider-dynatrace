4106b3e698225cc29de086761f42e9d778ebabf2
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2026-02-16 16:21:31 +0100

    feat(srg): Adds an E2E test example with links

---
+++ dynatrace/api/app/dynatrace/sitereliabilityguardian/testdata/terraform/example_with_links.tf
@@ -0,0 +1,36 @@
+resource "dynatrace_site_reliability_guardian" "#name#" {
+  name = "#name#"
+  tags = [ "stage:staging" ]
+  event_kind = "BIZ_EVENT"
+  objectives {
+    objective {
+      name                = "Error rate"
+      comparison_operator = "LESS_THAN_OR_EQUAL"
+      dql_query           =<<-EOT
+        fetch logs
+        | fieldsAdd errors = toLong(loglevel == "ERROR")
+        | summarize errorRate = sum(errors)/count() * 100
+      EOT
+      objective_type      = "DQL"
+      target              = 8
+      warning             = 6
+      links {
+        link {
+          url   = "https://www.dynatrace.com"
+          label = "Dynatrace"
+        }
+      }
+    }
+    objective {
+      name                = "Count bizevents"
+      comparison_operator = "GREATER_THAN_OR_EQUAL"
+      dql_query           =<<-EOT
+        fetch bizevents
+        | summarize count()
+      EOT
+      objective_type      = "DQL"
+      target              = 50
+      warning             = 55
+    }
+  }
+}
