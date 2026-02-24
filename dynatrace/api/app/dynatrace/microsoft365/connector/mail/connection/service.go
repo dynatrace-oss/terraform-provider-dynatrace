4615237cbfeadac6a889a04a849201aff01e6ade
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-02-24 12:44:27 +0100

    feat: update `dynatrace_ms365_email_connection` to 1.0.2
    Updated preconditions and description for enum fields.

--- dynatrace/api/app/dynatrace/microsoft365/connector/mail/connection/service.go
+++ dynatrace/api/app/dynatrace/microsoft365/connector/mail/connection/service.go
@@ -18,15 +18,15 @@
 package connection
 
 import (
-	connection "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/app/dynatrace/microsoft365/connector/mail/connection/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/app/dynatrace/microsoft365/connector/mail/connection/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.1"
+const SchemaVersion = "1.0.2"
 const SchemaID = "app:dynatrace.microsoft365.connector:mail.connection"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*connection.Settings] {
-	return settings20.Service[*connection.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
