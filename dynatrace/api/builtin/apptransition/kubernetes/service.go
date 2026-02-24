6a99f511d4c561d2b8064dabae6c12ccd5aef39e
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-02-24 12:45:52 +0100

    feat: update `dynatrace_kubernetes_app` to 1.1

--- dynatrace/api/builtin/apptransition/kubernetes/service.go
+++ dynatrace/api/builtin/apptransition/kubernetes/service.go
@@ -18,15 +18,15 @@
 package kubernetes
 
 import (
-	kubernetes "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/apptransition/kubernetes/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/apptransition/kubernetes/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.3"
+const SchemaVersion = "1.1"
 const SchemaID = "builtin:app-transition.kubernetes"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*kubernetes.Settings] {
-	return settings20.Service[*kubernetes.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
