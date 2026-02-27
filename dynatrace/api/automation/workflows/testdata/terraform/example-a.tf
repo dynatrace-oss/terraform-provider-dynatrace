1388d832ce759845996330aa216199ba5ab81c5d
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-02-27 13:17:47 +0100

    test: replace static IDs in automation examples
    - `example-a` now matches more the `example-a` of the upper folder. Also removed the other one as it's not needed or tested. That's why the order of the tasks in `testdata/example-a.tf` was changed, to match the one shown in the docs.
    - replaced static user IDs with dynamic ones that are created in an example

--- dynatrace/api/automation/workflows/testdata/terraform/example-a.tf
+++ dynatrace/api/automation/workflows/testdata/terraform/example-a.tf
@@ -1,19 +1,60 @@
-resource "dynatrace_automation_workflow" "#name#" {
-  description = "#name#"
-  actor       = "703d65c0-4aff-45d9-8b34-2c6f5f17bb8e"
-  owner       = "703d65c0-4aff-45d9-8b34-2c6f5f17bb8e"
-  private     = true
+resource "dynatrace_iam_service_user" "wf_user" {
+  name = "#name#"
+}
+
+resource "dynatrace_automation_workflow" "workflow_with_davis_event_trigger" {
+  description = "Desc"
+  actor       = dynatrace_iam_service_user.wf_user.id
+  # The owner can only be changed if the workflow is public (private = false).
+  owner       = dynatrace_iam_service_user.wf_user.id
+  private     = false
   title       = "#name#"
   tasks {
+    task {
+      name        = "http_request_1"
+      description = "Issue an HTTP request to any API"
+      action      = "dynatrace.automations:http-function"
+      active      = true
+      input       = jsonencode({
+        "method": "GET",
+        "url": "https://www.example.com/"
+      })
+      position {
+        x = 0
+        y = 1
+      }
+    }
+    task {
+      name        = "http_request_2"
+      description = "Issue an HTTP request to any API"
+      action      = "dynatrace.automations:http-function"
+      active      = false
+      timeout     = 50000
+      input       = jsonencode({
+        "method": "GET",
+        "url": "https://www.example.com/"
+      })
+      conditions {
+        custom = ""
+        states = {
+          http_request_1   = "SUCCESS"
+          run_javascript_1 = "OK"
+        }
+      }
+      position {
+        x = -1
+        y = 2
+      }
+    }
     task {
       name        = "http_request_3"
       description = "Issue an HTTP request to any API"
       action      = "dynatrace.automations:http-function"
       active      = false
       input       = jsonencode({
-              "method": "GET",
-              "url": "https://www.third-task.com"
-        })
+        "method": "GET",
+        "url": "https://www.example.com"
+      })
       conditions {
         custom = "{{http_request_1}}"
         states = {
@@ -31,59 +72,24 @@ resource "dynatrace_automation_workflow" "#name#" {
       action      = "dynatrace.automations:run-javascript"
       active      = false
       input       = jsonencode({
-              "script": "// optional import of sdk modules\nimport { execution } from '@dynatrace-sdk/automation-utils';\n\nexport default async function ({ execution_id }) {\n  // your code goes here\n  // e.g. get the current execution\n  const ex = await execution(execution_id);\n  console.log('Automated script execution on behalf of', ex.trigger);\n  \n  return { triggeredBy: ex.trigger };\n}"
-        })
+        "script": "// optional import of sdk modules\nimport { execution } from '@dynatrace-sdk/automation-utils';\n\nexport default async function ({ execution_id }) {\n  // your code goes here\n  // e.g. get the current execution\n  const ex = await execution(execution_id);\n  console.log('Automated script execution on behalf of', ex.trigger);\n  \n  return { triggeredBy: ex.trigger };\n}"
+      })
       position {
         x = -2
         y = 1
       }
     }
-    task {
-      name        = "http_request_1"
-      description = "Issue an HTTP request to any API"
-      action      = "dynatrace.automations:http-function"
-      active      = true
-      input       = jsonencode({
-              "method": "GET",
-              "url": "https://www.google.at/"
-        })
-      position {
-        x = 0
-        y = 1
-      }
-    }
-    task {
-      name        = "http_request_2"
-      description = "Issue an HTTP request to any API"
-      action      = "dynatrace.automations:http-function"
-      active      = false
-      input       = jsonencode({
-              "method": "GET",
-              "url": "https://www.second-task.com/"
-        })
-      conditions {
-        custom = ""
-        states = {
-          http_request_1   = "SUCCESS"
-          run_javascript_1 = "OK"
-        }
-      }
-      position {
-        x = -1
-        y = 2
-      }
-    }
   }
   trigger {
     event {
-      # active = false
+      active = false
       config {
         davis_event {
           entity_tags = {
             asdf = ""
           }
           entity_tags_match  = "all"
-          # on_problem_close = false
+          on_problem_close = false
           custom_filter = "matchesPhrase(custom.event.type, \"DEPLOY\")"
         }
       }
