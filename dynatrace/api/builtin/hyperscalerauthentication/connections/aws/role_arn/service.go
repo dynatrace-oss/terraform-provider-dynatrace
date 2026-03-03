04833c62cce434b930f2d361f31d5635dccc3c26
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-03-03 14:28:12 +0100

    fix: `dynatrace_aws_connection_role_arn` creation not working
    There was a dependency to the service of `dynatrace_aws_connection`. Because of that, the wrong update (create) logic was used to update the roleArn value and this lead to the role not being set at all. Therefore, we remove the dependency to the service of the other resource and use the default implementation

--- dynatrace/api/builtin/hyperscalerauthentication/connections/aws/role_arn/service.go
+++ dynatrace/api/builtin/hyperscalerauthentication/connections/aws/role_arn/service.go
@@ -22,7 +22,6 @@ import (
 	"errors"
 
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api"
-	awsconnection "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/hyperscalerauthentication/connections/aws"
 	role_arn "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/hyperscalerauthentication/connections/aws/role_arn/settings"
 	awsconnection_settings "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/hyperscalerauthentication/connections/aws/settings"
 	retrycommon "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/hyperscalerauthentication/connections/retry"
@@ -33,12 +32,13 @@ import (
 )
 
 const SchemaID = "builtin:hyperscaler-authentication.connections.aws"
-const SchemaVersion = "0.0.21"
+const SchemaVersion = "0.0.24"
 
 func Service(credentials *rest.Credentials) settings.CRUDService[*role_arn.Settings] {
 	return &service{
-		service:     settings20.Service[*role_arn.Settings](credentials, SchemaID, SchemaVersion),
-		connService: awsconnection.Service(credentials),
+		service: settings20.Service[*role_arn.Settings](credentials, SchemaID, SchemaVersion),
+		// We don't reuse awsconnection.Service(credentials), as this one contains custom update logic
+		connService: settings20.Service[*awsconnection_settings.Settings](credentials, SchemaID, SchemaVersion),
 	}
 }
 
