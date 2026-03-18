4c5cd3f892a82d836f1da13451cead824382d9e0
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-03-18 19:18:07 +0100

    feat: update `dynatrace_openpipeline_v2_*` resources to 1.54
    No changes for the provider

--- dynatrace/api/builtin/openpipeline/system/events/pipelinegroups/service.go
+++ dynatrace/api/builtin/openpipeline/system/events/pipelinegroups/service.go
@@ -24,7 +24,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.51"
+const SchemaVersion = "1.54"
 const SchemaID = "builtin:openpipeline.system.events.pipeline-groups"
 
 func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
