7fb19ce178fe9e0ccbe04fdf44f851f9a1b16004
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-03-18 19:06:22 +0100

    feat: update `dynatrace_web_app_enablement` to 1.8
    Nothing changed

--- dynatrace/api/builtin/rum/web/enablement/service.go
+++ dynatrace/api/builtin/rum/web/enablement/service.go
@@ -24,7 +24,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.6.1"
+const SchemaVersion = "1.8"
 const SchemaID = "builtin:rum.web.enablement"
 
 func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
