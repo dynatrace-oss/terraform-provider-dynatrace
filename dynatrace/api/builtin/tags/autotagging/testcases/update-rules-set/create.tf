e33ec889932d3e4244a21bea9dcb38e86f9d405a
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-09 17:07:30 +0200

    test: add typeSet testcase for dynatrace_autotag_v2

---
+++ dynatrace/api/builtin/tags/autotagging/testcases/update-rules-set/create.tf
@@ -0,0 +1,72 @@
+resource "dynatrace_autotag_v2" "tag" {
+  name = "#name#"
+  rules {
+    rule {
+      type                = "ME"
+      enabled             = true
+      value_format        = "{ProcessGroup:Environment:keptn_stage}"
+      value_normalization = "Leave text as-is"
+      attribute_rule {
+        entity_type                 = "SERVICE"
+        service_to_host_propagation = false
+        service_to_pgpropagation    = true
+        conditions {
+          condition {
+            dynamic_key        = "keptn_stage"
+            dynamic_key_source = "ENVIRONMENT"
+            key                = "PROCESS_GROUP_CUSTOM_METADATA"
+            operator           = "EXISTS"
+          }
+        }
+      }
+    }
+    rule {
+      type                = "ME"
+      enabled             = true
+      value_format        = "sprint"
+      value_normalization = "Leave text as-is"
+      attribute_rule {
+        entity_type = "SYNTHETIC_TEST"
+        conditions {
+          condition {
+            key      = "BROWSER_MONITOR_TAGS"
+            operator = "TAG_KEY_EQUALS"
+            tag      = "sprint"
+          }
+        }
+      }
+    }
+    rule {
+      type                = "ME"
+      enabled             = true
+      value_format        = "prod"
+      value_normalization = "Leave text as-is"
+      attribute_rule {
+        entity_type = "SYNTHETIC_TEST"
+        conditions {
+          condition {
+            key      = "BROWSER_MONITOR_TAGS"
+            operator = "TAG_KEY_EQUALS"
+            tag      = "prod"
+          }
+        }
+      }
+    }
+    rule {
+      type                = "ME"
+      enabled             = true
+      value_format        = "prod2"
+      value_normalization = "Leave text as-is"
+      attribute_rule {
+        entity_type = "SYNTHETIC_TEST"
+        conditions {
+          condition {
+            key      = "BROWSER_MONITOR_TAGS"
+            operator = "TAG_KEY_EQUALS"
+            tag      = "prod2"
+          }
+        }
+      }
+    }
+  }
+}
