739ef3d947c813e5aa97436644f75a5f0285994a
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-13 14:43:46 +0200

    fix(dynatrace_vulnerability_alerting): empty set bug
    The field `webhook_configuration->headers` leads to the Set-bug. Therefore, fixing it and adding a test for it.

---
+++ dynatrace/api/builtin/appsec/notificationintegration/testcases/update-webhook-headers-set/create.tf
@@ -0,0 +1,38 @@
+resource "dynatrace_vulnerability_alerting" "alert" {
+  name                   = "#name#"
+  enabled                = true
+  enabled_risk_levels    = ["LOW", "MEDIUM", "HIGH", "CRITICAL"]
+  enabled_trigger_events = ["SECURITY_PROBLEM_OPENED"]
+}
+
+resource "dynatrace_appsec_notification" "Terraform_Security_Problem_Webhook_Test" {
+  type                                    = "WEBHOOK"
+  enabled                                 = true
+  display_name                            = "Terraform Security Problem Webhook Test"
+  security_problem_based_alerting_profile = dynatrace_vulnerability_alerting.alert.id
+  trigger                                 = "SECURITY_PROBLEM"
+  security_problem_based_webhook_payload {
+    payload = jsonencode({
+      "DavisSecurityScore" : "{DavisSecurityScore}",
+      "SecurityProblemId" : "{SecurityProblemId}",
+      "SecurityProblemUrl" : "{SecurityProblemUrl}",
+      "Severity" : "{Severity}"
+    })
+  }
+  webhook_configuration {
+    accept_any_certificate = false
+    url                    = "https://www.dynatrace.com"
+    headers {
+      header {
+        name   = "http-header-name-01"
+        value  = "http-header-value-01"
+        secret = false
+      }
+      header {
+        name   = "http-header-name-02"
+        value  = "http-header-value-02"
+        secret = false
+      }
+    }
+  }
+}
---
+++ dynatrace/api/builtin/appsec/notificationintegration/testcases/update-webhook-headers-set/update.tf
@@ -0,0 +1,39 @@
+resource "dynatrace_vulnerability_alerting" "alert" {
+  name                   = "#name#"
+  enabled                = true
+  enabled_risk_levels    = ["LOW", "MEDIUM", "HIGH", "CRITICAL"]
+  enabled_trigger_events = ["SECURITY_PROBLEM_OPENED"]
+}
+
+resource "dynatrace_appsec_notification" "Terraform_Security_Problem_Webhook_Test" {
+  type                                    = "WEBHOOK"
+  enabled                                 = true
+  display_name                            = "Terraform Security Problem Webhook Test"
+  security_problem_based_alerting_profile = dynatrace_vulnerability_alerting.alert.id
+  trigger                                 = "SECURITY_PROBLEM"
+  security_problem_based_webhook_payload {
+    payload = jsonencode({
+      "DavisSecurityScore" : "{DavisSecurityScore}",
+      "SecurityProblemId" : "{SecurityProblemId}",
+      "SecurityProblemUrl" : "{SecurityProblemUrl}",
+      "Severity" : "{Severity}"
+    })
+  }
+  webhook_configuration {
+    accept_any_certificate = false
+    url                    = "https://www.dynatrace.com"
+    headers {
+      header {
+        name   = "http-header-name-01"
+        value  = "http-header-value-01"
+        secret = false
+      }
+      # modify => re-create due to set-hash change
+      header {
+        name   = "http-header-name-new"
+        secret = false
+        value  = "http-header-value-new"
+      }
+    }
+  }
+}
