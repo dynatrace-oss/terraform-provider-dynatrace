d6eb3667c189b28cbe4bd70cc6fa15b31766bda3
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-14 17:13:54 +0200

    test: add typeSet testcase for dynatrace_log_events

---
+++ dynatrace/api/builtin/logmonitoring/logevents/testcases/update-metadata-item-set/update.tf
@@ -0,0 +1,21 @@
+resource "dynatrace_log_events" "events" {
+  enabled = false
+  query   = "matchesPhrase(content, \"terratest\")"
+  summary = "Created by Terraform"
+  event_template {
+    description = "Created by Terraform"
+    event_type  = "INFO"
+    title       = "{content}"
+    metadata {
+      item {
+        metadata_key   = "terraform.key1"
+        metadata_value = "terraform.value1"
+      }
+      # updated
+      item {
+        metadata_key   = "terraform.keyEdit"
+        metadata_value = "terraform.valueEdit"
+      }
+    }
+  }
+}
