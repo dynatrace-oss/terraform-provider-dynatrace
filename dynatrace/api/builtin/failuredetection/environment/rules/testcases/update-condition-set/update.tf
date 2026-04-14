fe37ed616b464dddd946f01bb49563895ad8bec6
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-14 17:10:26 +0200

    test: add typeSet testcase for dynatrace_failure_detection_rules

---
+++ dynatrace/api/builtin/failuredetection/environment/rules/testcases/update-condition-set/update.tf
@@ -0,0 +1,68 @@
+resource "dynatrace_failure_detection_rules" "rule" {
+  name         = "#name#"
+  enabled      = true
+  parameter_id = dynatrace_failure_detection_parameters.parameter.id
+  conditions {
+    condition {
+      attribute = "SERVICE_NAME"
+      predicate {
+        case_sensitive = true
+        predicate_type = "STRING_EQUALS"
+        text_values    = [ "Terraform" ]
+      }
+    }
+    # update => re-create due to set-hash change
+    condition {
+      attribute = "PG_NAME"
+      predicate {
+        case_sensitive = true
+        predicate_type = "STRING_EQUALS"
+        text_values    = [ "Terraform-Service" ]
+      }
+    }
+  }
+}
+
+resource "dynatrace_failure_detection_parameters" "parameter" {
+  name        = "#name#"
+  description = "Created by Terraform"
+  broken_links {
+    http_404_not_found_failures = false
+  }
+  exception_rules {
+    ignore_all_exceptions         = false
+    ignore_span_failure_detection = true
+    custom_error_rules {
+      custom_error_rule {
+        request_attribute = "00000000-0000-0000-0000-000000000000"
+        condition {
+          compare_operation_type = "STRING_EXISTS"
+        }
+      }
+    }
+    custom_handled_exceptions {
+      custom_handled_exception {
+        class_pattern   = "ClassPattern"
+        message_pattern = "ExceptionPattern"
+      }
+    }
+    ignored_exceptions {
+      custom_handled_exception {
+        class_pattern   = "ClassPattern"
+        message_pattern = "ExceptionPattern"
+      }
+    }
+    success_forcing_exceptions {
+      custom_handled_exception {
+        class_pattern   = "ClassPattern"
+        message_pattern = "ExceptionPattern"
+      }
+    }
+  }
+  http_response_codes {
+    client_side_errors                        = "400-599"
+    fail_on_missing_response_code_client_side = false
+    fail_on_missing_response_code_server_side = true
+    server_side_errors                        = "500-599"
+  }
+}
