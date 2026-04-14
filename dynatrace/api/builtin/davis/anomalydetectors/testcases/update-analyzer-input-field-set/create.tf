52ca836b5bc348dfcd28c9a187b1482c397ffc71
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-14 17:04:30 +0200

    test: add typeSet testcase for dynatrace_davis_anomaly_detectors

---
+++ dynatrace/api/builtin/davis/anomalydetectors/testcases/update-analyzer-input-field-set/create.tf
@@ -0,0 +1,41 @@
+resource "dynatrace_davis_anomaly_detectors" "detector" {
+  description = "Sample Description"
+  enabled     = false
+  source      = "Davis Anomaly Detection"
+  title       = "#name#"
+  analyzer {
+    name = "dt.statistics.ui.anomaly_detection.StaticThresholdAnomalyDetectionAnalyzer"
+    input {
+      analyzer_input_field {
+        key   = "query"
+        value = "fetch bizevents"
+      }
+      analyzer_input_field {
+        key   = "threshold"
+        value = "12345678"
+      }
+    }
+  }
+  event_template {
+    properties {
+      property {
+        key   = "dt.source_entity"
+        value = "{dims:dt.source_entity}"
+      }
+      property {
+        key   = "event.type"
+        value = "CUSTOM_ALERT"
+      }
+      property {
+        key   = "event.name"
+        value = "Event Name Example"
+      }
+      property {
+        key   = "event.description"
+        value = "Event Description Example"
+      }
+    }
+  }
+  execution_settings {
+  }
+}
