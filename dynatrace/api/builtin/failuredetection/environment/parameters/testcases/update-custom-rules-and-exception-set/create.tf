99a6ecef182613353ce088dfcab21e04ab694a81
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-14 17:09:10 +0200

    fix(dynatrace_failure_detection_parameters): empty set bug
    The filter field (exception_rules ->custom_error_rules ->custom_error_rule) lead to the set bug.
    Fixing this one, adding a test

---
+++ dynatrace/api/builtin/failuredetection/environment/parameters/testcases/update-custom-rules-and-exception-set/create.tf
@@ -0,0 +1,61 @@
+resource "dynatrace_failure_detection_parameters" "params" {
+  name        = "#name#"
+  description = "Created by Terraform"
+  broken_links {
+    http_404_not_found_failures = false
+  }
+  exception_rules {
+    ignore_all_exceptions         = false
+    ignore_span_failure_detection = true
+    custom_error_rules {
+      custom_error_rule {
+        request_attribute = "00000000-0000-0000-0000-000000000000"
+        condition {
+          compare_operation_type = "STRING_EXISTS"
+        }
+      }
+      custom_error_rule {
+        request_attribute = "00000000-0000-0000-0000-000000000001"
+        condition {
+          compare_operation_type = "STRING_EXISTS"
+        }
+      }
+    }
+    custom_handled_exceptions {
+      custom_handled_exception {
+        class_pattern   = "ClassPattern"
+        message_pattern = "ExceptionPattern"
+      }
+      custom_handled_exception {
+        class_pattern   = "ClassPattern2"
+        message_pattern = "ExceptionPattern"
+      }
+    }
+    ignored_exceptions {
+      custom_handled_exception {
+        class_pattern   = "ClassPattern"
+        message_pattern = "ExceptionPattern"
+      }
+      custom_handled_exception {
+        class_pattern   = "ClassPattern2"
+        message_pattern = "ExceptionPattern"
+      }
+    }
+    success_forcing_exceptions {
+      custom_handled_exception {
+        class_pattern   = "ClassPattern"
+        message_pattern = "ExceptionPattern"
+      }
+      custom_handled_exception {
+        class_pattern   = "ClassPattern2"
+        message_pattern = "ExceptionPattern"
+      }
+    }
+  }
+  http_response_codes {
+    client_side_errors                        = "400-599"
+    fail_on_missing_response_code_client_side = false
+    fail_on_missing_response_code_server_side = true
+    server_side_errors                        = "500-599"
+  }
+}
