d185392cfc4b0fd34f9b2462f4cd56d05d83338b
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-15 17:42:25 +0200

    test: add typeSet testcase for dynatrace_mobile_app_request_errors

---
+++ dynatrace/api/builtin/rum/mobile/requesterrors/testcases/update-request-error-rule-set/update.tf
@@ -0,0 +1,16 @@
+data "dynatrace_mobile_application" "application" {
+  name = "Application"
+}
+
+resource "dynatrace_mobile_app_request_errors" "request_errors" {
+  scope = data.dynatrace_mobile_application.application.id
+  error_rules {
+    error_rule {
+      error_codes = "409"
+    }
+    # updated
+    error_rule {
+      error_codes = "410-500"
+    }
+  }
+}
