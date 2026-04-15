dd0b2ca3d1c5e76f751c992332f3b77b5a7d8a6d
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-15 17:39:03 +0200

    test: add typeSet testcase for dynatrace_os_services

---
+++ dynatrace/api/builtin/osservicesmonitoring/testcases/update-metadata-item-set/create.tf
@@ -0,0 +1,29 @@
+resource "dynatrace_os_services" "services" {
+  name                      = "#name#"
+  enabled                   = true
+  alert_activation_duration = 5
+  alerting                  = true
+  monitoring                = true
+  not_installed_alerting    = true
+  scope                     = "HOST-0000000000000000"
+  status_condition_linux    = "$eq(failed)"
+  system                    = "LINUX"
+  detection_conditions_linux {
+    linux_detection_condition {
+      condition = "$contains(Terraform)"
+      property  = "ServiceName"
+    }
+  }
+  metadata {
+    item {
+      metadata_key   = "TerraformKey1"
+      metadata_value = "TerraformValue1"
+    }
+    # to update
+    item {
+      metadata_key   = "TerraformKey2"
+      metadata_value = "TerraformValue2"
+    }
+  }
+}
+
