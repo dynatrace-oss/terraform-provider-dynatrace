c57300ec54bec89e68ceb3587ff2b42c4c1923e1
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-15 17:45:36 +0200

    test: add typeSet testcase for dynatrace_url_based_sampling

---
+++ dynatrace/api/builtin/urlbasedsampling/testcases/update-query-parameters-set/update.tf
@@ -0,0 +1,21 @@
+resource "dynatrace_url_based_sampling" "sampling" {
+  enabled              = true
+  factor               = "ReduceCapturingByFactor16"
+  http_method_any      = true
+  ignore               = false
+  path                 = "/examplepath"
+  path_comparison_type = "EQUALS"
+  query_parameters {
+    parameter {
+      name               = "QueryName1"
+      value              = "QueryValue1"
+      value_is_undefined = false
+    }
+    # updated
+    parameter {
+      name               = "QueryNameEdit"
+      value              = "QueryValueEdit"
+      value_is_undefined = false
+    }
+  }
+}
