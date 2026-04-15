85a51ef09952e37ff2da2e4b425f721d7e5eab60
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-15 17:44:28 +0200

    test: add typeSet testcase for dynatrace_browser_monitor_performance

--- dynatrace/api/builtin/synthetic/browser/performancethresholds/service_test.go
+++ dynatrace/api/builtin/synthetic/browser/performancethresholds/service_test.go
@@ -28,3 +28,7 @@ import (
 func TestAccBrowserMonitorPerformanceThresholds(t *testing.T) {
 	api.TestAcc(t)
 }
+
+func TestAccTestCasesBrowserMonitorPerformanceThresholds(t *testing.T) {
+	api.TestAccTestCases(t)
+}
