1b1f1f29036b9ecf96b63799dbaf7ecbf40afff5
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-15 17:43:10 +0200

    test: add typeSet testcase for dynatrace_rum_provider_breakdown

---
+++ dynatrace/api/builtin/rum/providerbreakdown/testcases/update-pattern-list-set/create.tf
@@ -0,0 +1,15 @@
+resource "dynatrace_rum_provider_breakdown" "breakdown" {
+  report_public_improvement = false
+  resource_name             = "#name#"
+  resource_type             = "ThirdParty"
+  domain_name_pattern_list {
+    domain_name_pattern {
+      pattern = "dynatrace.com/1"
+    }
+    # to update
+    domain_name_pattern {
+      pattern = "dynatrace.com/2"
+    }
+  }
+}
+
