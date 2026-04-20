273e2290288a039a13c5dd72565f5e04ac94efc3
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-20 14:32:07 +0200

    feat: update dynatrace_log_processing to 1.0.27
    No changes affecting our resource

--- dynatrace/api/builtin/logmonitoring/logdpprules/settings/settings.go
+++ dynatrace/api/builtin/logmonitoring/logdpprules/settings/settings.go
@@ -27,7 +27,7 @@ import (
 
 type Settings struct {
 	Enabled             bool                 `json:"enabled"`             // This setting is enabled (`true`) or disabled (`false`)
-	ProcessorDefinition *ProcessorDefinition `json:"ProcessorDefinition"` // ## Processor definition\nAdd a rule definition using our syntax. [In our documentation](https://dt-url.net/8k03xm2) you will find instructions and application [examples](https://dt-url.net/m24305t).
+	ProcessorDefinition *ProcessorDefinition `json:"ProcessorDefinition"` // ## Processor definition
 	Query               string               `json:"query"`               // Matcher
 	RuleName            string               `json:"ruleName"`            // Rule name
 	RuleTesting         *RuleTesting         `json:"RuleTesting"`         // ## Rule testing\n### 1. Paste a log / JSON sample
@@ -47,12 +47,11 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		},
 		"processor_definition": {
 			Type:        schema.TypeList,
-			Description: "## Processor definition\nAdd a rule definition using our syntax. [In our documentation](https://dt-url.net/8k03xm2) you will find instructions and application [examples](https://dt-url.net/m24305t).",
+			Description: "## Processor definition",
 			Required:    true,
-
-			Elem:     &schema.Resource{Schema: new(ProcessorDefinition).Schema()},
-			MinItems: 1,
-			MaxItems: 1,
+			Elem:        &schema.Resource{Schema: new(ProcessorDefinition).Schema()},
+			MinItems:    1,
+			MaxItems:    1,
 		},
 		"query": {
 			Type:             schema.TypeString,
@@ -67,12 +66,11 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		},
 		"rule_testing": {
 			Type:        schema.TypeList,
-			Description: "## Rule testing\n### 1. Paste a log / JSON sample",
+			Description: "## Rule testing\n ### 1. Paste a log / JSON sample",
 			Required:    true,
-
-			Elem:     &schema.Resource{Schema: new(RuleTesting).Schema()},
-			MinItems: 1,
-			MaxItems: 1,
+			Elem:        &schema.Resource{Schema: new(RuleTesting).Schema()},
+			MinItems:    1,
+			MaxItems:    1,
 		},
 		"insert_after": {
 			Type:        schema.TypeString,
