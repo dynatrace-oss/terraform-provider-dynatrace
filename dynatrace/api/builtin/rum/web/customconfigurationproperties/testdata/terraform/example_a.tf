3ba7be801adc49bf548ff016c39ba42fb13e1b4d
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-21 10:36:21 +0200

    test: fix dynatrace_web_app_custom_config_properties race condition
    This adds `#name#` for the custom_property that needs to be unique, so that this one will not conflict with other parallel test executions

--- dynatrace/api/builtin/rum/web/customconfigurationproperties/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/rum/web/customconfigurationproperties/testdata/terraform/example_a.tf
@@ -1,4 +1,4 @@
 resource "dynatrace_web_app_custom_config_properties" "#name#" {
   application_id  = "APPLICATION-1234567890000000"
-  custom_property = "examplekey=examplevalue"
+  custom_property = "examplekey=#name#"
 }
