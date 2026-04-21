b35f963e65af2d85f7da30584234efd269965237
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-21 13:39:06 +0200

    test(dynatrace_hub_extension_active_version): align extension usage
    This updates the dynatrace_hub_extension_active_version example to use the same extension (`com.dynatrace.extension.jmx-weblogic-cp`) as other examples

--- dynatrace/api/v2/hub/extension/active_version/testdata/terraform/example-a.tf
+++ dynatrace/api/v2/hub/extension/active_version/testdata/terraform/example-a.tf
@@ -1,4 +1,4 @@
-resource "dynatrace_hub_extension_active_version" "custom_com_dynatrace_extension_prometheus-cadvisor" {
-  name    = "com.dynatrace.extension.active-directory-python"
-  version = "3.1.6"
+resource "dynatrace_hub_extension_active_version" "jmx-weblogic-cp" {
+  name    = "com.dynatrace.extension.jmx-weblogic-cp"
+  version = "2.1.1"
 }
