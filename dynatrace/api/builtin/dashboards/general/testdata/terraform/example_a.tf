31bd5a869d3a4a1e9b4220bbc267dbc9e226131e
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-22 19:02:55 +0200

    test: replace static dashboard IDs
    Instead of using hardcoded classic dashboard and group IDs ,we now create one on demand

--- dynatrace/api/builtin/dashboards/general/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/dashboards/general/testdata/terraform/example_a.tf
@@ -1,9 +1,43 @@
-resource "dynatrace_dashboards_general" "#name#" {
+resource "dynatrace_dashboards_general" "general" {
+  depends_on = [time_sleep.dashboard_create]
   enable_public_sharing = false
   default_dashboard_list {
     default_dashboard {
-      dashboard = "41eae96d-4930-4f44-bbd8-3699f21a8bbf"
-      user_group = "d0c2d3e3-c1b4-456a-b0ce-c560273f1488"
+      dashboard = dynatrace_dashboard.dashboard.id
+      user_group = dynatrace_iam_group.group.id
     }
+  }
+}
+
+# the datasource of dynatrace_dashboards_general may not be updated yet.
+resource "time_sleep" "dashboard_create" {
+  depends_on = [dynatrace_dashboard.dashboard]
+  create_duration = "5s"
+}
+
+resource "dynatrace_iam_group" "group" {
+  name = "#name#"
+}
+
+resource "dynatrace_dashboard" "dashboard" {
+  dashboard_metadata {
+    name   = "#name#"
+    owner  = "Dynatrace"
+    tags   = ["Kubernetes"]
+    dynamic_filters {
+      filters = ["KUBERNETES_CLUSTER"]
+    }
+  }
+  tile {
+    name       = "Markdown"
+    tile_type  = "MARKDOWN"
+    configured = true
+    bounds {
+      top    = 0
+      width  = 684
+      height = 38
+      left   = 0
+    }
+    markdown = "## Cluster resource overview"
   }
 }
