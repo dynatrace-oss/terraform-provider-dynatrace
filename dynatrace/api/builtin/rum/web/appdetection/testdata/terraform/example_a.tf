1822b1c06a5a240dbb4b2857f7f9e9142ac30014
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-22 19:04:27 +0200

    test: replace static application IDs
    This replaces static `APPLICATION-` IDs with dynamic created ones

--- dynatrace/api/builtin/rum/web/appdetection/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/rum/web/appdetection/testdata/terraform/example_a.tf
@@ -1,5 +1,67 @@
-resource "dynatrace_application_detection_rule_v2" "#name#" {
-  application_id = "APPLICATION-90CE01F27D579187"
+resource "dynatrace_application_detection_rule_v2" "detection_rule" {
+  application_id = dynatrace_web_application.application.id
   matcher        = "DOMAIN_MATCHES"
   pattern        = "TerraformTest"
+}
+
+resource "dynatrace_web_application" "application" {
+  name                                 = "#name#"
+  type                                 = "AUTO_INJECTED"
+  cost_control_user_session_percentage = 100
+  load_action_key_performance_metric   = "VISUALLY_COMPLETE"
+  real_user_monitoring_enabled         = true
+  xhr_action_key_performance_metric    = "VISUALLY_COMPLETE"
+  custom_action_apdex_settings {
+    frustrating_fallback_threshold = 12000
+    frustrating_threshold          = 12000
+    tolerated_fallback_threshold   = 3000
+    tolerated_threshold            = 3000
+  }
+  load_action_apdex_settings {
+    frustrating_fallback_threshold = 12000
+    frustrating_threshold          = 12000
+    tolerated_fallback_threshold   = 3000
+    tolerated_threshold            = 3000
+  }
+  monitoring_settings {
+    add_cross_origin_anonymous_attribute = true
+    cache_control_header_optimizations   = true
+    injection_mode                       = "JAVASCRIPT_TAG"
+    script_tag_cache_duration_in_hours   = 1
+    advanced_javascript_tag_settings {
+      max_action_name_length = 100
+      max_errors_to_capture  = 10
+      additional_event_handlers {
+        max_dom_nodes = 5000
+      }
+    }
+    content_capture {
+      resource_timing_settings {
+        instrumentation_delay    = 53
+        non_w3c_resource_timings = true
+        w3c_resource_timings     = true
+      }
+      timeout_settings {
+        temporary_action_limit         = 3
+        temporary_action_total_timeout = 100
+        timed_action_support           = true
+      }
+    }
+  }
+  user_action_naming_settings {}
+  waterfall_settings {
+    resource_browser_caching_threshold            = 50
+    resources_threshold                           = 100000
+    slow_cnd_resources_threshold                  = 200000
+    slow_first_party_resources_threshold          = 200000
+    slow_third_party_resources_threshold          = 200000
+    speed_index_visually_complete_ratio_threshold = 50
+    uncompressed_resources_threshold              = 860
+  }
+  xhr_action_apdex_settings {
+    frustrating_fallback_threshold = 12000
+    frustrating_threshold          = 12000
+    tolerated_fallback_threshold   = 3000
+    tolerated_threshold            = 3000
+  }
 }
