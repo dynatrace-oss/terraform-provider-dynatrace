188f572e8a9ddbbc88a2c5ac50a0a0d2fede9a0f
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-22 19:09:12 +0200

    test: change generic settings example
    Instead of using a custom app that may not exist, we now use a builtin setting we already have, in this case, the SRG

--- dynatrace/api/builtin/generic/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/generic/testdata/terraform/example_a.tf
@@ -1,12 +1,23 @@
 resource "dynatrace_generic_setting" "ABC" {
-  schema = "app:my.booking.analytics:connection"
+  schema = "app:dynatrace.site.reliability.guardian:guardians"
   scope  = "environment"
   value = jsonencode({
-    "client_id"     : "asdfhh",
-    "client_secret" : "mysecret",
-    "name"          : "ABC",
-    "tenant_id"     : "asdf",
-    "type"          : "client_secret",
-    "user_id"       : "asdf"
+    "name": "#name#",
+    "tags": [ "stage:staging" ],
+    "eventKind": "BIZ_EVENT",
+    "objectives": [
+      {
+        "name": "Error rate",
+        "comparisonOperator": "LESS_THAN_OR_EQUAL",
+        "dqlQuery": <<-EOT
+        fetch logs
+        | fieldsAdd errors = toLong(loglevel == "ERROR")
+        | summarize errorRate = sum(errors)/count() * 100
+      EOT
+        "objectiveType": "DQL",
+        "target": 8,
+        "warning": 6
+      }
+    ]
   })
 }
