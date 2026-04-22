842b6b34c7398f6db36053aeb1a7d85dc0328887
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-22 19:08:08 +0200

    test: replace static object IDs
    Instead of having a static object ID, dynatrace_autotag_rules and dynatrace_appsec_notification now use the ID of the related resource

--- dynatrace/api/builtin/appsec/notificationintegration/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/appsec/notificationintegration/testdata/terraform/example_a.tf
@@ -2,7 +2,7 @@ resource "dynatrace_appsec_notification" "Terraform_Security_Problem_Webhook_Tes
   type                                    = "WEBHOOK"
   enabled                                 = true
   display_name                            = "Terraform Security Problem Webhook Test"
-  security_problem_based_alerting_profile = "vu9U3hXa3q0AAAABACxidWlsdGluOmFwcHNlYy5ub3RpZmljYXRpb24tYWxlcnRpbmctcHJvZmlsZQAGdGVuYW50AAZ0ZW5hbnQAJDMyMDhkNWMyLTFlZmYtMzk5My1iNjMwLWI0MjQ5N2U4MDQ2Nr7vVN4V2t6t"
+  security_problem_based_alerting_profile = dynatrace_vulnerability_alerting.alert.id
   trigger                                 = "SECURITY_PROBLEM"
   security_problem_based_webhook_payload {
     payload = jsonencode({
@@ -16,4 +16,11 @@ resource "dynatrace_appsec_notification" "Terraform_Security_Problem_Webhook_Tes
     accept_any_certificate = false
     url                    = "https://www.dynatrace.com"
   }
+}
+
+resource "dynatrace_vulnerability_alerting" "alert" {
+  name                   = "#name#"
+  enabled                = true
+  enabled_risk_levels    = ["LOW", "MEDIUM", "HIGH", "CRITICAL"]
+  enabled_trigger_events = ["SECURITY_PROBLEM_OPENED"]
 }
