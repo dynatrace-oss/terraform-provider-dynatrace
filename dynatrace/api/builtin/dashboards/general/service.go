34fbf47e53fa79ac3bcada8987a3947594b961a4
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-27 17:37:50 +0200

    fix: flaky dashboard general creation
    Adds a custom retry for dynatrace_dashboards_general, where its data source may not up to date

--- dynatrace/api/builtin/dashboards/general/service.go
+++ dynatrace/api/builtin/dashboards/general/service.go
@@ -18,15 +18,83 @@
 package general
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api"
 	general "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/dashboards/general/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 )
 
 const SchemaVersion = "1.0.18"
 const SchemaID = "builtin:dashboards.general"
+const defaultTimeout = 20 * time.Second
+
+type service struct {
+	service settings.CRUDService[*general.Settings]
+}
 
 func Service(credentials *rest.Credentials) settings.CRUDService[*general.Settings] {
-	return settings20.Service[*general.Settings](credentials, SchemaID, SchemaVersion)
+	return &service{settings20.Service[*general.Settings](credentials, SchemaID, SchemaVersion)}
+}
+func (me *service) Create(ctx context.Context, v *general.Settings) (*api.Stub, error) {
+	var apiStub *api.Stub
+	err := retry.RetryContext(ctx, defaultTimeout, func() *retry.RetryError {
+		var err error
+		apiStub, err = me.service.Create(ctx, v)
+		return classifyRetryError(err)
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return apiStub, nil
+}
+
+// classifyRetryErrors retries on certain conflicts
+// During "create" the data source of the dashboard may not be up to date.
+func classifyRetryError(err error) *retry.RetryError {
+	if err == nil {
+		return nil
+	}
+	if restError, ok := errors.AsType[rest.Error](err); ok && restError.Code == http.StatusBadRequest {
+		containsConflict := slices.ContainsFunc(restError.ConstraintViolations, func(cv rest.ConstraintViolation) bool {
+			return strings.Contains(cv.Message, "Invalid value in datasource")
+		})
+		if containsConflict {
+			return retry.RetryableError(err)
+		}
+	}
+
+	return retry.NonRetryableError(err)
+}
+
+func (me *service) Update(ctx context.Context, id string, v *general.Settings) error {
+	return retry.RetryContext(ctx, defaultTimeout, func() *retry.RetryError {
+		err := me.service.Update(ctx, id, v)
+		return classifyRetryError(err)
+	})
+}
+
+func (me *service) Delete(ctx context.Context, id string) error {
+	return me.service.Delete(ctx, id)
+}
+
+func (me *service) Get(ctx context.Context, id string, v *general.Settings) error {
+	return me.service.Get(ctx, id, v)
+}
+
+func (me *service) List(ctx context.Context) (api.Stubs, error) {
+	return me.service.List(ctx)
+}
+
+func (me *service) SchemaID() string {
+	return SchemaID
 }
