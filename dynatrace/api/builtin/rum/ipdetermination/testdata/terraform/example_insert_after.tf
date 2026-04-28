de43d08b6b9e5166eee9e70466389078e6a44240
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-04-28 14:01:47 +0200

    test: fix example uniqueness
    There were potential conflicts when running dynatrace_rum_ip_determination and dynatrace_automation_workflow_k8s_connections in parallel.
    Using our placeholder `#name#` to make the unique properties unique

--- dynatrace/api/builtin/rum/ipdetermination/testdata/terraform/example_insert_after.tf
+++ dynatrace/api/builtin/rum/ipdetermination/testdata/terraform/example_insert_after.tf
@@ -1,8 +1,8 @@
 resource "dynatrace_rum_ip_determination" "first-instance" {
-  header_name = "X-Header-Example3"
+  header_name = "X-Header-#name#-3"
 }
 
 resource "dynatrace_rum_ip_determination" "second-instance" {
-  header_name  = "X-Header-Example4"
+  header_name  = "X-Header-#name#-4"
   insert_after = dynatrace_rum_ip_determination.first-instance.id
 }
