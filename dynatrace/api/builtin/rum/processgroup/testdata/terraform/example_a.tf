608f5adf69eef3590d3c078c986bcb056ce8dc68
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-11 13:30:22 +0200

    test: use data source for process groups
    Instead of setting the process group ID via variable/env, a data source is now used.
    Previously, during the research, the wrong/deprecated data source was used for process groups that faded out after 3 days. The data source `dynatrace_entity` doesn't have this problem

--- dynatrace/api/builtin/rum/processgroup/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/rum/processgroup/testdata/terraform/example_a.tf
@@ -1,9 +1,9 @@
-variable "PROCESS_GROUP_ID" {
-  type = string
+data "dynatrace_entity" "process_group" {
+  entity_selector = "type(\"PROCESS_GROUP\")"
 }
 
 
 resource "dynatrace_process_group_rum" "rum" {
   enable           = false
-  process_group_id = var.PROCESS_GROUP_ID
+  process_group_id = data.dynatrace_entity.process_group.id
 }
