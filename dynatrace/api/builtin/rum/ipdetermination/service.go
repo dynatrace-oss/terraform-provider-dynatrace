0336d2621786e495b93a0f16d8d7a5d6790f7987
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-20 19:52:54 +0200

    feat(dynatrace_rum_ip_determination): updated to v1.0.2
    No changes affecting the resource

--- dynatrace/api/builtin/rum/ipdetermination/service.go
+++ dynatrace/api/builtin/rum/ipdetermination/service.go
@@ -18,15 +18,15 @@
 package ipdetermination
 
 import (
-	ipdetermination "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/ipdetermination/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/ipdetermination/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.1"
+const SchemaVersion = "1.0.2"
 const SchemaID = "builtin:rum.ip-determination"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*ipdetermination.Settings] {
-	return settings20.Service[*ipdetermination.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
