66be241ea87a0e04a8b21b88f38cd67bca2a90c0
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 15:49:23 +0200

    feat(dynatrace_web_app_beacon_origins): updated to v1.0.4
    Description fixes

--- dynatrace/api/builtin/rum/web/beacondomainorigins/service.go
+++ dynatrace/api/builtin/rum/web/beacondomainorigins/service.go
@@ -18,15 +18,15 @@
 package beacondomainorigins
 
 import (
-	beacondomainorigins "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/beacondomainorigins/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/beacondomainorigins/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.3"
+const SchemaVersion = "1.0.4"
 const SchemaID = "builtin:rum.web.beacon-domain-origins"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*beacondomainorigins.Settings] {
-	return settings20.Service[*beacondomainorigins.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
