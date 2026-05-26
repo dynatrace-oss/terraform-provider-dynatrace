654cfe623fb644b31440f325f4b6e7a4ea46cf98
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 16:36:48 +0200

    feat(dynatrace_web_app_resource_cleanup): updated to v1.0.7
    No changes affecting the resource

--- dynatrace/api/builtin/rum/web/resourcecleanuprules/service.go
+++ dynatrace/api/builtin/rum/web/resourcecleanuprules/service.go
@@ -18,15 +18,15 @@
 package resourcecleanuprules
 
 import (
-	resourcecleanuprules "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/resourcecleanuprules/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/resourcecleanuprules/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.6"
+const SchemaVersion = "1.0.7"
 const SchemaID = "builtin:rum.web.resource-cleanup-rules"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*resourcecleanuprules.Settings] {
-	return settings20.Service[*resourcecleanuprules.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
