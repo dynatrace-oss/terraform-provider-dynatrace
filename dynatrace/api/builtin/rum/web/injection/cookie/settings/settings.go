345979a34d4344aa00046ca971eb50a6f4990951
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 16:16:57 +0200

    feat(dynatrace_web_app_injection_cookie): updated to v1.0.2
    Description fixes

--- dynatrace/api/builtin/rum/web/injection/cookie/settings/settings.go
+++ dynatrace/api/builtin/rum/web/injection/cookie/settings/settings.go
@@ -25,7 +25,7 @@ import (
 type Settings struct {
 	ApplicationID            string                  `json:"-" scope:"applicationId"`         // The scope of this settings. If the settings should cover the whole environment, just don't specify any scope.
 	CookiePlacementDomain    *string                 `json:"cookiePlacementDomain,omitempty"` // Specify an alternative domain for cookies set by Dynatrace. Keep in mind that your browser may not allow placement of cookies on certain domains (for example, top-level domains). Before typing a domain name here, confirm that the domain will accept cookies from your browser. For details, see the list of [forbidden top-level domains](https://dt-url.net/9n6b0pfz).
-	SameSiteCookieAttribute  SameSiteCookieAttribute `json:"sameSiteCookieAttribute"`         // Possible Values: `LAX`, `NONE`, `NOTSET`, `STRICT`
+	SameSiteCookieAttribute  SameSiteCookieAttribute `json:"sameSiteCookieAttribute"`         // Define if your cookie should be restricted to a first-party or same-site context. Learn more about [SameSite cookies and available values](https://dt-url.net/yds1p8u). Possible values: `LAX`, `NONE`, `NOTSET`, `STRICT`
 	UseSecureCookieAttribute bool                    `json:"useSecureCookieAttribute"`        // If your application is only accessible via SSL, you can add the Secure attribute to all cookies set by Dynatrace. This setting prevents the display of warnings from PCI-compliance security scanners. Be aware that with this setting enabled Dynatrace correlation of user actions with server-side web requests is only possible over SSL connections.
 }
 
@@ -47,7 +47,7 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		},
 		"same_site_cookie_attribute": {
 			Type:        schema.TypeString,
-			Description: "Possible Values: `LAX`, `NONE`, `NOTSET`, `STRICT`",
+			Description: "Define if your cookie should be restricted to a first-party or same-site context. Learn more about [SameSite cookies and available values](https://dt-url.net/yds1p8u). Possible values: `LAX`, `NONE`, `NOTSET`, `STRICT`",
 			Required:    true,
 		},
 		"use_secure_cookie_attribute": {
