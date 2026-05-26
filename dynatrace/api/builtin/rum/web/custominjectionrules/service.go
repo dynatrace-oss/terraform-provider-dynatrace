69146830a20c84fe8de3a603dca4b589fc6958f4
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 16:14:51 +0200

    feat(dynatrace_web_app_custom_injection): updated to v1.0.2
    BREAKING CHANGE
    
    - Description updates and fixes
    - Correctly handle conditional fields
    
    - 'html_pattern' must not be specified unless 'rule' is one of ['BeforeSpecificHtml', 'AfterSpecificHtml']
    - 'url_pattern' must not be specified unless 'operator' is one of ['Equals', 'Starts', 'Ends', 'Contains']

--- dynatrace/api/builtin/rum/web/custominjectionrules/service.go
+++ dynatrace/api/builtin/rum/web/custominjectionrules/service.go
@@ -18,15 +18,15 @@
 package custominjectionrules
 
 import (
-	custominjectionrules "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/custominjectionrules/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/custominjectionrules/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.1"
+const SchemaVersion = "1.0.2"
 const SchemaID = "builtin:rum.web.custom-injection-rules"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*custominjectionrules.Settings] {
-	return settings20.Service[*custominjectionrules.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
