a34144cf05cdc70276665fabcac7c52f6b2e9b81
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 15:53:49 +0200

    feat(dynatrace_web_app_custom_prop_restrictions): updated to v0.4.1
    Description fixes

--- dynatrace/api/builtin/rum/web/capturecustomproperties/service.go
+++ dynatrace/api/builtin/rum/web/capturecustomproperties/service.go
@@ -24,7 +24,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.4"
+const SchemaVersion = "0.4.1"
 const SchemaID = "builtin:rum.web.capture-custom-properties"
 
 func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
