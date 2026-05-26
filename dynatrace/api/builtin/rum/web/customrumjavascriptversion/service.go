91a163d7532762231171e1aaf6165727dbe065f1
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 16:16:19 +0200

    feat(dynatrace_web_app_javascript_version): updated to v1.1.1
    No updates in the schema

--- dynatrace/api/builtin/rum/web/customrumjavascriptversion/service.go
+++ dynatrace/api/builtin/rum/web/customrumjavascriptversion/service.go
@@ -18,15 +18,15 @@
 package customrumjavascriptversion
 
 import (
-	customrumjavascriptversion "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/customrumjavascriptversion/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/customrumjavascriptversion/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.1"
+const SchemaVersion = "1.1.1"
 const SchemaID = "builtin:rum.web.custom-rum-javascript-version"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*customrumjavascriptversion.Settings] {
-	return settings20.Service[*customrumjavascriptversion.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
