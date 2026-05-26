78adf0657e6dda275124b2b30e180778395e1f33
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 15:50:41 +0200

    feat(dynatrace_web_app_beacon_endpoint): updated to v1.0.2
    BREAKING CHANGE
    
    - Description fixes
    - Correctly handle conditional fields
    
    - 'url' must not be specified unless 'type' is set to 'ONEAGENT'
    - 'use_cors' must not be specified unless 'type' is set to 'ONEAGENT'

--- dynatrace/api/builtin/rum/web/beaconendpoint/service.go
+++ dynatrace/api/builtin/rum/web/beaconendpoint/service.go
@@ -18,15 +18,15 @@
 package beaconendpoint
 
 import (
-	beaconendpoint "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/beaconendpoint/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/beaconendpoint/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.0.1"
+const SchemaVersion = "1.0.2"
 const SchemaID = "builtin:rum.web.beacon-endpoint"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*beaconendpoint.Settings] {
-	return settings20.Service[*beaconendpoint.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
