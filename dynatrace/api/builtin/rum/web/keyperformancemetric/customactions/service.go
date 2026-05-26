9f4df41b51b5ca7fc3e04f2d4fd23e073be20e79
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 16:23:15 +0200

    feat(dynatrace_web_app_key_performance_custom): updated to v1.1.1
    No changes in the schema

--- dynatrace/api/builtin/rum/web/keyperformancemetric/customactions/service.go
+++ dynatrace/api/builtin/rum/web/keyperformancemetric/customactions/service.go
@@ -18,15 +18,15 @@
 package customactions
 
 import (
-	customactions "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/keyperformancemetric/customactions/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/rum/web/keyperformancemetric/customactions/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.1"
+const SchemaVersion = "1.1.1"
 const SchemaID = "builtin:rum.web.key-performance-metric-custom-actions"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*customactions.Settings] {
-	return settings20.Service[*customactions.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
