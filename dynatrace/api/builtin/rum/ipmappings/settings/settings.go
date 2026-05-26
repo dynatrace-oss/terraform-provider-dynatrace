4a415fd5e8c8dc396338959990fe95b89351a80c
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-05-26 15:40:05 +0200

    feat(dynatrace_rum_ip_locations): updated to v1.1.3
    BREAKING CHANGE
    
    - Description updates
    - Correctly handle conditional required fields
    
    - 'latitude' must not be specified unless 'city' is set
    - 'longitude' must not be specified unless 'city' is set

--- dynatrace/api/builtin/rum/ipmappings/settings/settings.go
+++ dynatrace/api/builtin/rum/ipmappings/settings/settings.go
@@ -18,18 +18,21 @@
 package ipmappings
 
 import (
+	"fmt"
+
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/opt"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/terraform/hcl"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 type Settings struct {
 	City        *string  `json:"city,omitempty"`       // The city name of the location.
-	CountryCode string   `json:"countryCode"`          // The country code of the location. \n\n Use the alpha-2 code of the [ISO 3166-2 standard](https://dt-url.net/iso3166-2), (for example, `AT` for Austria or `PL` for Poland).
+	CountryCode string   `json:"countryCode"`          // The country code of the location. \n\n   Use the alpha-2 code of the [ISO 3166-2 standard](https://dt-url.net/iso3166-2), (for example, `AT` for Austria or `PL` for Poland).
 	Ip          string   `json:"ip"`                   // Single IP or IP range start address
 	IpTo        *string  `json:"ipTo,omitempty"`       // IP range end
 	Latitude    *float64 `json:"latitude,omitempty"`   // Latitude
 	Longitude   *float64 `json:"longitude,omitempty"`  // Longitude
-	RegionCode  *string  `json:"regionCode,omitempty"` // The region code of the location. \n\n For the [USA](https://dt-url.net/iso3166us) or [Canada](https://dt-url.net/iso3166ca) use ISO 3166-2 state codes without `US-` or `CA-` prefix. \n\n For the rest of the world use [FIPS 10-4 codes](https://dt-url.net/fipscodes) without country prefix.
+	RegionCode  *string  `json:"regionCode,omitempty"` // The region code of the location. \n\n   For the [USA](https://dt-url.net/iso3166us) or [Canada](https://dt-url.net/iso3166ca) use ISO 3166-2 state codes without `US-` or `CA-` prefix. \n\n   For the rest of the world use [FIPS 10-4 codes](https://dt-url.net/fipscodes) without country prefix.
 }
 
 func (me *Settings) Name() string {
@@ -41,11 +44,11 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		"city": {
 			Type:        schema.TypeString,
 			Description: "The city name of the location.",
-			Optional:    true,
+			Optional:    true, // nullable
 		},
 		"country_code": {
 			Type:        schema.TypeString,
-			Description: "The country code of the location. \n\n Use the alpha-2 code of the [ISO 3166-2 standard](https://dt-url.net/iso3166-2), (for example, `AT` for Austria or `PL` for Poland).",
+			Description: "The country code of the location. \n\n   Use the alpha-2 code of the [ISO 3166-2 standard](https://dt-url.net/iso3166-2), (for example, `AT` for Austria or `PL` for Poland).",
 			Required:    true,
 		},
 		"ip": {
@@ -56,22 +59,22 @@ func (me *Settings) Schema() map[string]*schema.Schema {
 		"ip_to": {
 			Type:        schema.TypeString,
 			Description: "IP range end",
-			Optional:    true,
+			Optional:    true, // nullable
 		},
 		"latitude": {
 			Type:        schema.TypeFloat,
 			Description: "Latitude",
-			Optional:    true,
+			Optional:    true, // precondition
 		},
 		"longitude": {
 			Type:        schema.TypeFloat,
 			Description: "Longitude",
-			Optional:    true,
+			Optional:    true, // precondition
 		},
 		"region_code": {
 			Type:        schema.TypeString,
-			Description: "The region code of the location. \n\n For the [USA](https://dt-url.net/iso3166us) or [Canada](https://dt-url.net/iso3166ca) use ISO 3166-2 state codes without `US-` or `CA-` prefix. \n\n For the rest of the world use [FIPS 10-4 codes](https://dt-url.net/fipscodes) without country prefix.",
-			Optional:    true,
+			Description: "The region code of the location. \n\n   For the [USA](https://dt-url.net/iso3166us) or [Canada](https://dt-url.net/iso3166ca) use ISO 3166-2 state codes without `US-` or `CA-` prefix. \n\n   For the rest of the world use [FIPS 10-4 codes](https://dt-url.net/fipscodes) without country prefix.",
+			Optional:    true, // nullable
 		},
 	}
 }
@@ -88,8 +91,24 @@ func (me *Settings) MarshalHCL(properties hcl.Properties) error {
 	})
 }
 
+func (me *Settings) HandlePreconditions() error {
+	if (me.Latitude == nil) && (me.City != nil) {
+		me.Latitude = new(0.0)
+	}
+	if (me.Longitude == nil) && (me.City != nil) {
+		me.Longitude = new(0.0)
+	}
+	if (me.Latitude != nil) && (me.City == nil) {
+		return fmt.Errorf("'latitude' must not be specified unless 'city' is set; got 'city'='%v'", opt.ValOrNil(me.City))
+	}
+	if (me.Longitude != nil) && (me.City == nil) {
+		return fmt.Errorf("'longitude' must not be specified unless 'city' is set; got 'city'='%v'", opt.ValOrNil(me.City))
+	}
+	return nil
+}
+
 func (me *Settings) UnmarshalHCL(decoder hcl.Decoder) error {
-	err := decoder.DecodeAll(map[string]any{
+	return decoder.DecodeAll(map[string]any{
 		"city":         &me.City,
 		"country_code": &me.CountryCode,
 		"ip":           &me.Ip,
@@ -98,13 +117,4 @@ func (me *Settings) UnmarshalHCL(decoder hcl.Decoder) error {
 		"longitude":    &me.Longitude,
 		"region_code":  &me.RegionCode,
 	})
-	if me.City != nil {
-		if me.Latitude == nil {
-			me.Latitude = new(0.0)
-		}
-		if me.Longitude == nil {
-			me.Longitude = new(0.0)
-		}
-	}
-	return err
 }
