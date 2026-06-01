3c91b2fe9041ae6ac861077d144e3b1012ca7e23
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2026-06-01 14:32:27 +0200

    feat: add dynatrace_gcp_connection resource
    Adds the dynatrace_gcp_connection Terraform resource backed by the builtin:hyperscaler-authentication.connections.gcp Settings 2.0 schema. Registering the resource in the provider and the export descriptor will happen in a follow-up. Includes a GCP e2e acceptance test that provisions a service account and IAM binding before creating the connection. The test is skipped for now. It will be enabled in a follow-up.

---
+++ dynatrace/api/builtin/hyperscalerauthentication/connections/gcp/settings/service_account_impersonation.go
@@ -0,0 +1,58 @@
+/**
+* @license
+* Copyright 2026 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package gcp
+
+import (
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/terraform/hcl"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type ServiceAccountImpersonation struct {
+	Consumers        []ConsumersOfServiceAccountImpersonation `json:"consumers,omitempty"`        // Dynatrace integrations that can use this connection. Possible values: `SVC:com.dynatrace.bo`, `SVC:com.dynatrace.da`, `SVC:com.dynatrace.openpipeline`
+	ServiceAccountID *string                                  `json:"serviceAccountId,omitempty"` // Id of your Service Account
+}
+
+func (me *ServiceAccountImpersonation) Schema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"consumers": {
+			Type:        schema.TypeList,
+			Description: "Dynatrace integrations that can use this connection. Possible values: `SVC:com.dynatrace.bo`, `SVC:com.dynatrace.da`, `SVC:com.dynatrace.openpipeline`",
+			Optional:    true, // minobjects == 0
+			Elem:        &schema.Schema{Type: schema.TypeString},
+		},
+		"service_account_id": {
+			Type:        schema.TypeString,
+			Description: "Id of your Service Account",
+			Optional:    true, // nullable
+		},
+	}
+}
+
+func (me *ServiceAccountImpersonation) MarshalHCL(properties hcl.Properties) error {
+	return properties.EncodeAll(map[string]any{
+		"consumers":          me.Consumers,
+		"service_account_id": me.ServiceAccountID,
+	})
+}
+
+func (me *ServiceAccountImpersonation) UnmarshalHCL(decoder hcl.Decoder) error {
+	return decoder.DecodeAll(map[string]any{
+		"consumers":          &me.Consumers,
+		"service_account_id": &me.ServiceAccountID,
+	})
+}
---
+++ dynatrace/api/builtin/hyperscalerauthentication/connections/gcp/testdata/terraform/e2e_test.tf
@@ -0,0 +1,48 @@
+variable "gcp_project_id" {
+  type        = string
+  description = "The Google Cloud Platform project ID"
+}
+
+# Configure the GCP provider
+provider "google" {
+  project = var.gcp_project_id
+}
+
+# Create a Service Account in GCP
+resource "google_service_account" "gcp_service_account" {
+  account_id   = "#name#"
+  display_name = "#name#"
+}
+
+# Fetch the DT GCP Principal
+data "dynatrace_gcp_principal" "principal" {
+}
+
+# Grant DT GCP Principal access to the service account
+resource "google_service_account_iam_member" "wif_binding" {
+  service_account_id = google_service_account.gcp_service_account.name
+  member  = "serviceAccount:${data.dynatrace_gcp_principal.principal.principal}"
+  role    = "roles/iam.serviceAccountTokenCreator"
+}
+
+# Wait for IAM binding to propagate
+resource time_sleep "wait_for_iam_binding" {
+  depends_on = [google_service_account_iam_member.wif_binding]
+  create_duration = "2m"
+}
+
+# Create GCP connection
+resource "dynatrace_gcp_connection" "example" {
+  name = "#name#"
+  type = "serviceAccountImpersonation"
+  service_account_impersonation {
+    service_account_id  = google_service_account.gcp_service_account.email
+    consumers = [
+      "SVC:com.dynatrace.da"
+    ]
+  }
+
+  depends_on = [
+    time_sleep.wait_for_iam_binding
+  ]
+}
