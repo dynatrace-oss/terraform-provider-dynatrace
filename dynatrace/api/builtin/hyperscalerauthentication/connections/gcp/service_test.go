96b70b25a79e2a1aa8bd1711eec663d7eebb6edb
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2026-06-01 14:32:27 +0200

    feat: add dynatrace_gcp_connection resource
    Adds the dynatrace_gcp_connection Terraform resource backed by the builtin:hyperscaler-authentication.connections.gcp Settings 2.0 schema. Registering the resource in the provider and the export descriptor will happen in a follow-up. Includes a GCP e2e acceptance test that provisions a service account and IAM binding before creating the connection. The test is skipped for now. It will be enabled in a follow-up.

---
+++ dynatrace/api/builtin/hyperscalerauthentication/connections/gcp/service_test.go
@@ -0,0 +1,38 @@
+//go:build integration
+
+/**
+* @license
+* Copyright 2026 Dynatrace LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package gcp_test
+
+import (
+	"testing"
+
+	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/testing/api"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestGcpConnection(t *testing.T) {
+	t.Skip("Enable this test as soon as GCP test environment setup is done.")
+
+	api.TestAcc(t, api.TestAccOptions{
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"google": {VersionConstraint: "7.33.0", Source: "hashicorp/google"},
+			"time":   {VersionConstraint: "0.14.0", Source: "hashicorp/time"},
+		},
+	})
+}
