cfc74637d6eb7a2f0417072f5df76ac9f55a86a1
Arthur Pitman <arthur.pitman@dynatrace.com>
2026-06-12 14:46:23 +0200

    fix: Actually update resource descriptor

--- dynatrace/export/resource_descriptor.go
+++ dynatrace/export/resource_descriptor.go
@@ -556,6 +556,7 @@ func AddInsertAfterWeakIDDependencies(resources map[ResourceType]ResourceDescrip
 			continue
 		}
 		descriptor.Dependencies = append(descriptor.Dependencies, Dependencies.WeakID(resType))
+		resources[resType] = descriptor
 	}
 }
 
