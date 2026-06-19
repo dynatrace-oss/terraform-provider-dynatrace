ac8a1c74becf6f4c7ccd249464738aa1ab1acc27
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-06-19 11:42:24 +0200

    chore: fix license header year
    Correctly sets the year to 2026

--- dynatrace/api/builtin/openpipeline/davis/problems/dataforwarding/service_unit_test.go
+++ dynatrace/api/builtin/openpipeline/davis/problems/dataforwarding/service_unit_test.go
@@ -2,7 +2,7 @@
 
 /**
 * @license
-* Copyright 2025 Dynatrace LLC
+* Copyright 2026 Dynatrace LLC
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
