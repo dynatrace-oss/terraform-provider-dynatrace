be3661d07fb4f5cec67193e272d12070fc2ea772
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-06-30 09:24:08 +0200

    feat: updated dynatrace_kubernetes to v3.1.1
    No changes in the schema affecting our resource
    
    Correctly handle preconditions.
    New conditions:
    - 'active_gate_group' must not be specified unless 'cluster_id_enabled' is set to 'false'
    - 'auth_token' must not be specified unless 'cluster_id_enabled' is set to 'false'
    - 'certificate_check_enabled' must not be specified unless 'cluster_id_enabled' is set to 'false'
    - 'cluster_id' must not be specified unless 'cluster_id_enabled' is set to 'true'
    - 'endpoint_url' must not be specified unless 'cluster_id_enabled' is set to 'false'
    - 'hostname_verification_enabled' must not be specified unless 'cluster_id_enabled' is set to 'false'

--- dynatrace/api/builtin/cloud/kubernetes/service.go
+++ dynatrace/api/builtin/cloud/kubernetes/service.go
@@ -18,15 +18,15 @@
 package kubernetes
 
 import (
-	kubernetes "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/cloud/kubernetes/settings"
+	service "github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/api/builtin/cloud/kubernetes/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/rest"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings"
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "3.1"
+const SchemaVersion = "3.1.1"
 const SchemaID = "builtin:cloud.kubernetes"
 
-func Service(credentials *rest.Credentials) settings.CRUDService[*kubernetes.Settings] {
-	return settings20.Service[*kubernetes.Settings](credentials, SchemaID, SchemaVersion)
+func Service(credentials *rest.Credentials) settings.CRUDService[*service.Settings] {
+	return settings20.Service[*service.Settings](credentials, SchemaID, SchemaVersion)
 }
