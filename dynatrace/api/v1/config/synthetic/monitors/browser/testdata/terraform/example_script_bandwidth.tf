e8f7b1206eb674b26a7f634763f3fcc2617623b1
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-07-22 15:39:21 +0200

    fix: dynatrace_browser_monitor should consider 0 for bandwidth fields
    Because of the Terraform plugin SDK limitation, we can't differentiate between "not set" and 0; a value of "0" is set to nil.
    Coincidentally, there is actually an "OR" condition behind the bandwidth interface. With that, we can determine whether the value should be set to 0 or omitted, since all properties are required. It's either `network_type`, or all of `latency`, `download`, and `upload` (mutually exclusive)

---
+++ dynatrace/api/v1/config/synthetic/monitors/browser/testdata/terraform/example_script_bandwidth.tf
@@ -0,0 +1,154 @@
+data "dynatrace_synthetic_location" "location" {
+  name = "Location"
+}
+
+data "dynatrace_application" "web_application" {
+  name = "Web Application"
+}
+
+resource "dynatrace_credentials" "credentials_vault" {
+  name        = "#name#"
+  description = "my credentials vault"
+  scopes      = ["SYNTHETIC"]
+  username    = "username"
+  password    = "password"
+}
+
+# check that bandwidth with 0 values is working
+resource "dynatrace_browser_monitor" "monitor" {
+  name                   = "#name#"
+  frequency              = 15
+  locations              = [data.dynatrace_synthetic_location.location.id]
+  manually_assigned_apps = [data.dynatrace_application.web_application.id]
+  anomaly_detection {
+    loading_time_thresholds {
+      enabled = true
+    }
+    outage_handling {
+      global_outage  = true
+      retry_on_error = true
+      global_outage_policy {
+        consecutive_runs = 1
+      }
+    }
+  }
+  key_performance_metrics {
+    load_action_kpm = "VISUALLY_COMPLETE"
+    xhr_action_kpm  = "VISUALLY_COMPLETE"
+  }
+  script {
+    type = "clickpath"
+    configuration {
+      bypass_csp = true
+      user_agent = "Mozilla"
+      bandwidth {
+        latency  = 0
+        upload   = 0
+        download = 0
+      }
+      device {
+        name        = "Apple iPhone 8"
+        orientation = "landscape"
+      }
+      headers {
+        header {
+          name  = "kjh"
+          value = "kjh"
+        }
+      }
+      ignored_error_codes {
+        status_codes = "400"
+      }
+      javascript_setttings {
+        timeout_settings {
+          action_limit  = 3
+          total_timeout = 100
+        }
+        visually_complete_options {
+          image_size_threshold = 0
+          inactivity_timeout   = 0
+          mutation_timeout     = 0
+        }
+      }
+    }
+    events {
+      event {
+        description = "Loading of \"https://example.com\""
+        navigate {
+          url = "https://example.com"
+          authentication {
+            type  = "http_authentication"
+            creds = dynatrace_credentials.credentials_vault.id
+          }
+          wait {
+            wait_for = "page_complete"
+          }
+        }
+      }
+      event {
+        description = "jhjhjh"
+        navigate {
+          url = "https://example.com"
+          authentication {
+            type  = "http_authentication"
+            creds = dynatrace_credentials.credentials_vault.id
+          }
+          validate {
+            validation {
+              type  = "text_match"
+              match = "kkl"
+              regex = true
+              target {
+                window = "k"
+              }
+            }
+          }
+          wait {
+            timeout  = 60000
+            wait_for = "validation"
+            validation {
+              type  = "element_match"
+              match = "kjkj"
+              target {
+                locators {
+                  locator {
+                    type  = "css"
+                    value = "jjj"
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      event {
+        description = "fvf"
+        click {
+          button = 0
+          validate {
+            validation {
+              type = "text_match"
+            }
+          }
+          wait {
+            wait_for = "page_complete"
+          }
+        }
+      }
+      event {
+        description = "jsfoo"
+        javascript {
+          code = <<-EOT
+            let x = 3;
+            for (var i = 0; i < x; x++) {
+                console.log("asdf");
+            }
+          EOT
+          wait {
+            wait_for = "page_complete"
+          }
+        }
+      }
+    }
+  }
+}
