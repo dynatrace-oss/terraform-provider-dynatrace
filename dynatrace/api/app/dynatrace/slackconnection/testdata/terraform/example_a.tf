119a4a7f8d2d01580ddd3dbd891004892bcb2e6f
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-07-27 17:11:51 +0200

    feat: update dynatrace_automation_workflow_slack to v0.2
    New fields `external_approval` and `signing_secret`

--- dynatrace/api/app/dynatrace/slackconnection/testdata/terraform/example_a.tf
+++ dynatrace/api/app/dynatrace/slackconnection/testdata/terraform/example_a.tf
@@ -1,4 +1,11 @@
-resource "dynatrace_automation_workflow_slack" "#name#" {
+resource "dynatrace_automation_workflow_slack" "default" {
   name  = "#name#"
   token = "#######"
+}
+
+resource "dynatrace_automation_workflow_slack" "external_approval" {
+  name              = "#name#"
+  token             = "#######"
+  external_approval = true
+  signing_secret    = "#######"
 }
