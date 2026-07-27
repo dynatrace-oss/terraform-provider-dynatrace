e8bdd958fd081cbccc4fbf745e8e52b191f2e934
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-07-27 18:41:36 +0200

    feat: update dynatrace_service_splitting to v0.0.16
    No changes in the schema

--- dynatrace/api/builtin/servicesplittingrules/service.go
+++ dynatrace/api/builtin/servicesplittingrules/service.go
@@ -24,7 +24,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "0.0.15"
+const SchemaVersion = "0.0.16"
 const SchemaID = "builtin:service-splitting-rules"
 
 func Service(clientSet rest.ClientSet) (settings.CRUDService[*service.Settings], error) {
