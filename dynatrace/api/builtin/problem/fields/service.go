6094d8256dd978032525757ee379c54190f6f85c
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-08-24 19:27:50 +0200

    feat: update `dynatrace_problem_fields` to v1.11
    No changes in the schema affecting our resource

--- dynatrace/api/builtin/problem/fields/service.go
+++ dynatrace/api/builtin/problem/fields/service.go
@@ -24,7 +24,7 @@ import (
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/dynatrace/settings/services/settings20"
 )
 
-const SchemaVersion = "1.10"
+const SchemaVersion = "1.11"
 const SchemaID = "builtin:problem.fields"
 
 func Service(clientSet rest.ClientSet) (settings.CRUDService[*service.Settings], error) {
