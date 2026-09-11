a8f01ceebec78ff9b1f040c98da4d5cbd519a2e4
Dominic Weinberger <dominic.weinberger@dynatrace.com>
2026-09-11 17:36:59 +0200

    fix(synthetic): Tighten dynatrace_browser_monitor validation
    When validation type is `text_match` or `content_type`, then `match` must be set. This is now validated. Also, `text_match` validation type is now documented.

--- dynatrace/api/v1/config/synthetic/monitors/browser/settings/event/validation.go
+++ dynatrace/api/v1/config/synthetic/monitors/browser/settings/event/validation.go
@@ -18,6 +18,8 @@
 package event
 
 import (
+	"fmt"
+
 	"github.com/dynatrace-oss/terraform-provider-dynatrace/terraform/hcl"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -49,23 +51,23 @@ func (me *Validations) UnmarshalHCL(decoder hcl.Decoder) error {
 }
 
 type Validation struct {
-	Type        ValidationType `json:"type"`              // The goal of the validation. `content_match` (check page for the specific content. Not allowed for validation inside of wait condition), `element_match` (check page for the specific element)
-	Match       string         `json:"match"`             // The content to look for on the page.\nRegular expressions are allowed. In that case set `isRegex` as `true`. Required for `content_match`, optional for `element_match`.
-	IsRegex     bool           `json:"isRegex,omitempty"` // Defines whether `match` is plain text (`false`) of a regular expression (`true`)
+	Type        ValidationType `json:"type"`              // The goal of the validation. `content_match` (check page for the specific content. Not allowed for validation inside of wait condition), `element_match` (check page for the specific element), `text_match` (check page for the specific text)
+	Match       string         `json:"match"`             // The content to look for on the page. Regular expressions are allowed. In that case set `isRegex` as `true`. Required for `content_match` and `text_match`, optional for `element_match`.
+	IsRegex     bool           `json:"isRegex,omitempty"` // Defines whether `match` is plain text (`false`) or a regular expression (`true`)
 	FailIfFound bool           `json:"failIfFound"`       // The condition of the validation. `false` means the validation succeeds if the specified content/element is found. `true` means the validation fails if the specified content/element is found
-	Target      *Target        `json:"target,omitempty"`  // The elemnt to look for on the page
+	Target      *Target        `json:"target,omitempty"`  // The element to look for on the page
 }
 
 func (me *Validation) Schema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
 		"type": {
 			Type:        schema.TypeString,
-			Description: "The goal of the validation. `content_match` (check page for the specific content. Not allowed for validation inside of wait condition), `element_match` (check page for the specific element).",
+			Description: "The goal of the validation. `content_match` (check page for the specific content. Not allowed for validation inside of wait condition), `element_match` (check page for the specific element), `text_match` (check page for the specific text).",
 			Required:    true,
 		},
 		"match": {
 			Type:        schema.TypeString,
-			Description: "The content to look for on the page.\nRegular expressions are allowed. In that case set `isRegex` as `true`. Required for `content_match`, optional for `element_match`.",
+			Description: "The content to look for on the page. Regular expressions are allowed. In that case set `isRegex` as `true`. Required for `content_match` and `text_match`, optional for `element_match`.",
 			Optional:    true,
 		},
 		"regex": {
@@ -127,14 +129,23 @@ func (me *Validation) UnmarshalHCL(decoder hcl.Decoder) error {
 	return nil
 }
 
-// ValidationType The goal of the validation. `content_match` (check page for the specific content. Not allowed for validation inside of wait condition), `element_match` (check page for the specific element)
+func (me *Validation) HandlePreconditions() error {
+	if (me.Type == ValidationTypes.ContentMatch || me.Type == ValidationTypes.TextMatch) && len(me.Match) == 0 {
+		return fmt.Errorf("'match' must be a non-empty string when 'type' is '%s'", me.Type)
+	}
+	return nil
+}
+
+// ValidationType The goal of the validation. `content_match` (check page for the specific content. Not allowed for validation inside of wait condition), `element_match` (check page for the specific element), `text_match` (check page for the specific text)
 type ValidationType string
 
 // ValidationTypes offers the known enum values
 var ValidationTypes = struct {
 	ContentMatch ValidationType
 	ElementMatch ValidationType
+	TextMatch    ValidationType
 }{
 	`content_match`,
 	`element_match`,
+	`text_match`,
 }
