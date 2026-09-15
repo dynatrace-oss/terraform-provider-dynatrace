e9abe91414134e87e068eaf2a8c4d9ba3ad77714
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2026-09-15 16:05:21 +0200

    test: make integration tests more stable
    Due to concurrency issues or missing cleanups, some configs can't be redeployed.
    Therefore, we add a unique identifier to the affected tests, so that they won't run into uniqueness constraints.
    Affected resources
    - dynatrace_user_session_metrics
    - dynatrace_user_action_metrics
    - dynatrace_custom_units
    - dynatrace_remote_environments
    - dynatrace_web_app_beacon_origins

--- dynatrace/api/builtin/remote/environment/testdata/terraform/example_a.tf
+++ dynatrace/api/builtin/remote/environment/testdata/terraform/example_a.tf
@@ -1,6 +1,6 @@
-resource "dynatrace_remote_environments" "#name#" {
-  name          = "TerraformExample"
+resource "dynatrace_remote_environments" "env" {
+  name          = "#name#"
   network_scope = "EXTERNAL"
   token         = "################"
-  uri           = "https://terraformexample.live.dynatrace.com"
+  uri           = "https://example-#name#.live.dynatrace.com"
 }
